e49a86dd972adc1fd8915e61bab7812b3615043b
Jezreel Christian Bacasno <bacasnojezreel@gmail.com>
2024-04-18 01:07:59 +0800

    @86ep4n65d #5th Commit - Regular SATA is renamed as RegularSATAQuestionaire  with Api Integration ft DND With All types as any

--- apps/simulator/src/components/blocks/CaseStudy/CaseStudyContainer.tsx
+++ apps/simulator/src/components/blocks/CaseStudy/CaseStudyContainer.tsx
@@ -1,21 +1,94 @@
+// import React from "react";
+// import { QuestionaireProps, CaseStudyProps } from "@/core/types/ssrData";
+// import { useAlertMessageV2 } from "@repo/utils/contexts/AlertMessageContext";
+// import { HCPQuestion, MRSNQuestion, DDCQuestion, DDTQuestion, MCQCSQuestionnaire, DNDQuestionaire } from "./CaseStudyQuestions";
+
+
+
+// import { SATAQuestionaire } from "./CaseStudyQuestions/SATAQuestionaire";
+// export const CaseStudyContainer: React.FC<CaseStudyProps> = ({
+//   questionaire,
+// }) => {
+//   const { AlertMessage } = useAlertMessageV2();
+
+//   if (questionaire.length > 0) {
+//     const deserializeContents: any =
+//       questionaire?.length > 0 &&
+//       questionaire?.filter((cms: QuestionaireProps) => {
+//         return cms.QType === "SATA";
+//       });
+
+//     const {
+//       QType: QuestionType,
+//       answer,
+//       hasAlert,
+//       qId,
+//     } = deserializeContents?.[0];
+
+//     if (hasAlert) {
+//       return (
+//         <>
+//           <AlertMessage
+//             severity="info"
+//             title={`Case Study: Item ${qId}`}
+//             description=""
+//           />
+//           {renderSwitch(QuestionType, deserializeContents, answer)}
+//         </>
+//       );
+//     } else {
+//       return renderSwitch(QuestionType, deserializeContents, answer);
+//     }
+//   }
+//   return <h3>No questionaire Loaded</h3>;
+// };
+
+// function renderSwitch(
+//   QuestionType: string,
+//   deserializeContents: any,
+//   answer: any
+// ) {
+//   switch (QuestionType) {
+
+//     case "SATA":
+//       return <SATAQuestionaire questionaire={deserializeContents} />
+//     case "MCQNoGroup":
+//     case "MCQGroup":
+//       return <MCQCSQuestionnaire questionaire={deserializeContents} />
+//     case "HCP":
+//       return <HCPQuestion questionaire={deserializeContents} answer={answer} />
+//     case "MRSN":
+//       return <MRSNQuestion questionaire={deserializeContents} answer={answer} />
+//     case "DDC":
+//       return <DDCQuestion questionaire={deserializeContents} answer={answer} />
+//     case "DDT":
+//       return <DDTQuestion questionaire={deserializeContents} answer={answer} />
+//     case "DND1":
+//       return <DNDQuestionaire questionaire={deserializeContents} />
+
+//     default:
+//       return <h3>No questionaire Loaded</h3>;
+//   }
+// }
+
+
+
 import React from "react";
 import { QuestionaireProps, CaseStudyProps } from "@/core/types/ssrData";
 import { useAlertMessageV2 } from "@repo/utils/contexts/AlertMessageContext";
 import { HCPQuestion, MRSNQuestion, DDCQuestion, DDTQuestion, MCQCSQuestionnaire, DNDQuestionaire } from "./CaseStudyQuestions";
 
-
-
-import { SATAQuestionaire } from "./CaseStudyQuestions/SATAQuestionaire";
 export const CaseStudyContainer: React.FC<CaseStudyProps> = ({
   questionaire,
 }) => {
+  console.log(questionaire)
   const { AlertMessage } = useAlertMessageV2();
 
   if (questionaire.length > 0) {
     const deserializeContents: any =
       questionaire?.length > 0 &&
       questionaire?.filter((cms: QuestionaireProps) => {
-        return cms.QType === "SATA";
+        return cms.QType === "DND1";
       });
 
     const {
@@ -50,10 +123,10 @@ function renderSwitch(
 ) {
   switch (QuestionType) {
 
-    case "SATA":
-      return <SATAQuestionaire questionaire={deserializeContents} />
-    case "MCQNoGroup":
-    case "MCQGroup":
+    // case "SATA":
+    //   return <SATAQuestionaire questionaire={deserializeContents} />
+    // case "MCQNoGroup":
+    case "MCQGroup" || "MCQNoGroup":
       return <MCQCSQuestionnaire questionaire={deserializeContents} />
     case "HCP":
       return <HCPQuestion questionaire={deserializeContents} answer={answer} />
@@ -70,4 +143,3 @@ function renderSwitch(
       return <h3>No questionaire Loaded</h3>;
   }
 }
-
--- apps/simulator/src/components/parse-contents.tsx
+++ apps/simulator/src/components/parse-contents.tsx
@@ -1,115 +1,38 @@
 
-// import React from "react";
-// import { McqQuestion, CaseStudyContainer, RegularSATAQuestionaire, } from "./blocks";
-// import { useSimulatorGlobals } from "@/core/context/SimulatorContext";
-// import { datatypes } from "@repo/utils";
-
-// interface Props {
-//   questionKey: string;
-//   itemSelected: datatypes.CalcItemSelectValues[]
-// }
-
-// export const ParseContents: React.FC<Props> = ({
-//   questionKey,
-//   itemSelected
-// }) => {
-//   /* use this contents to get the content data */
-//   const { contents } = useSimulatorGlobals()
-//   if (contents && contents.answerUI?.length > 0 && contents.choices?.length > 0 && contents.questionType?.length > 0) {
-//     const qKey = contents.questionType.filter(key => key.qType === questionKey);
-//     const {
-//       qType: QuestionType
-//     } = qKey[0];
-//     switch (QuestionType) {
-//       case "SATA":
-//         return <RegularSATAQuestionaire contents={contents} itemselection={itemSelected} />
-//       case "MCQ":
-//         return <McqQuestion questionaire={[]} answer={[]} />
-//       case "CaseStudy":
-//         return <CaseStudyContainer questionaire={[]} />
-//       default:
-//         return <h3>No questionaire Loaded</h3>;
-
-//     }
-//   } else {
-//     return <h3>No questionaire Loaded</h3>;
-//   }
-// };
-
 import React from "react";
-import { McqQuestion, CaseStudyContainer, RegularSATAQuestionaire } from "./blocks";
+import { McqQuestion, CaseStudyContainer, RegularSATAQuestionaire, } from "./blocks";
 import { useSimulatorGlobals } from "@/core/context/SimulatorContext";
 import { datatypes } from "@repo/utils";
-import { SsrMockQuestionaire } from "@/core/types/ssrData";
 
 interface Props {
   questionKey: string;
   itemSelected: datatypes.CalcItemSelectValues[]
-  questionaire: SsrMockQuestionaire[];
 }
 
 export const ParseContents: React.FC<Props> = ({
   questionKey,
-  itemSelected,
-  questionaire
+  itemSelected
 }) => {
   /* use this contents to get the content data */
   const { contents } = useSimulatorGlobals()
   if (contents && contents.answerUI?.length > 0 && contents.choices?.length > 0 && contents.questionType?.length > 0) {
     const qKey = contents.questionType.filter(key => key.qType === questionKey);
-    // const {
-    //   qType: QuestionType
-    // } = qKey[0];
-
-    const deserializeContents: any =
-      questionaire?.length > 0 &&
-      questionaire?.filter((cms: SsrMockQuestionaire) => {
-        return cms.QType === questionKey;
-      });
-
     const {
-      QType,
-      questions,
-      answer,
-    } = deserializeContents?.[0];
-
-
-    switch (QType) {
+      qType: QuestionType
+    } = qKey[0];
+    switch (QuestionType) {
       case "SATA":
         return <RegularSATAQuestionaire contents={contents} itemselection={itemSelected} />
       case "MCQ":
-        return <McqQuestion questionaire={deserializeContents} answer={answer} />
+        return <McqQuestion questionaire={[]} answer={[]} />
       case "CaseStudy":
-        return <CaseStudyContainer questionaire={deserializeContents} />
+        return <CaseStudyContainer questionaire={[]} />
       default:
         return <h3>No questionaire Loaded</h3>;
 
     }
   } else {
-    // return <h3>No questionaire Loaded</h3>;
-    const deserializeContents: any =
-      questionaire?.length > 0 &&
-      questionaire?.filter((cms: SsrMockQuestionaire) => {
-        return cms.QType === questionKey;
-      });
-
-    const {
-      QType,
-      questions,
-      answer,
-    } = deserializeContents?.[0];
-
-
-    switch (QType) {
-      case "SATA":
-        return <RegularSATAQuestionaire contents={contents} itemselection={itemSelected} />
-      case "MCQ":
-        return <McqQuestion questionaire={deserializeContents} answer={answer} />
-      case "CaseStudy":
-        return <CaseStudyContainer questionaire={questions} />
-      default:
-        return <h3>No questionaire Loaded</h3>;
-
-    }
+    return <h3>No questionaire Loaded</h3>;
   }
 };
+
