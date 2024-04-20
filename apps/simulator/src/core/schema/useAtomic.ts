d7fafd20f0d25e1431f1b81e8c16b74eb5d3abd8
Jacob Allen <vjacoballen3020@gmail.com>
2024-04-20 13:52:55 +0800

    @86ep32g8z Single-Select validation ft ui refactorization and type renaming

--- apps/simulator/src/core/schema/useAtomic.ts
+++ apps/simulator/src/core/schema/useAtomic.ts
@@ -1,6 +1,11 @@
+import { McqSsValidationType } from '@/core/schema/mcq/validation';
 import { MrsnValidationType } from "./mrsn/validation";
 import { atom } from "jotai";
 
 export const MrsnValidationAtom = atom<MrsnValidationType | undefined>(
+  undefined
+);
+
+export const McqSsValidationAtom = atom<McqSsValidationType | undefined>(
   undefined
 );
