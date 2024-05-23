eef03bbbd9b62e1bcb06c16de772cbf0bcfa6edd
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2024-05-23 20:51:46 +0800

    @86epck6t1 #Web backoffice project setup

---
+++ apps/web-backoffice/src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import type { AppProps } from "next/app";
+import { NextPage } from "next";
+import { ReactElement, ReactNode } from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./index.css";
+import "./mui.css";
+
+export type NextPageWithLayout<P = any, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  return getLayout(<Component {...pageProps} />);
+}
