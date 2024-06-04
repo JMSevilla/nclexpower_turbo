b2330afc1a032594e3181627a660f6f29063f73e
Jezreel Christian Bacasno <bacasnojezreel@gmail.com>
2024-06-05 03:03:19 +0800

    fix/86eph07ze/MCQ-CaseStudy-Type-Fixed

--- apps/simulator/src/components/blocks/CaseStudy/CaseStudyQuestions/MCQCSQuestionnaire.tsx
+++ apps/simulator/src/components/blocks/CaseStudy/CaseStudyQuestions/MCQCSQuestionnaire.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { SsrData } from "@/core/types/ssrData";
 import { MCQAnswerGroupTable } from '@/components/blocks/CaseStudy/CaseStudyQuestions/MCQTable/MCQAnswerGroupTable';
 
-export const MCQCSQuestionnaire: React.FC<SsrData> = ({ questionaire }) => {
+export const MCQCSQuestionnaire: React.FC<SsrData> = ({ questionaire, answer }) => {
     return (
         <div className="p-2 py-2 h-full w-full">
             <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
@@ -43,7 +43,7 @@ export const MCQCSQuestionnaire: React.FC<SsrData> = ({ questionaire }) => {
                     </div>
                 </Grid>
                 <Grid item xs={12} sm={6} md={6} lg={6}>
-                    <MCQAnswerGroupTable questionaire={questionaire} />
+                    <MCQAnswerGroupTable questionaire={questionaire} answer={answer} />
                 </Grid>
 
             </Grid>
