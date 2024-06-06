03fca6211c461f51047f849bc4a1b796ec9ecab9
Jezreel Christian Bacasno <bacasnojezreel@gmail.com>
2024-06-07 00:48:25 +0800

    @86epcnkxy #Username & Password Validation Comment Addressed added deleted file to commit

--- packages/core-library/components/blocks/LoginBlock/validation.ts
+++
@@ -1,9 +0,0 @@
-import * as Yup from 'yup'
-
-export const loginSchema = Yup.object({
-    email: Yup.string().email("Input a valid email").required("Email is required").default(''),
-    password: Yup.string().min(8, "Must be at least 8 characters").required("Password is required").default(''),
-    rememberMe: Yup.boolean().default(false)
-});
-
-export type LoginValidationType = Yup.InferType<typeof loginSchema>;
