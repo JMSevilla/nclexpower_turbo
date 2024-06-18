96b3132f44d471b80ba7bdf2a5156f2d5bb176b4
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2024-06-18 21:08:30 +0800

    @hotfix/r1-dev #fix api concatination

--- apps/simulator/next.config.js
+++ apps/simulator/next.config.js
@@ -11,7 +11,10 @@ module.exports = {
   publicRuntimeConfig: {
     processEnv: {
       ...Object.fromEntries(Object.entries(process.env).filter(([key]) => key.includes('NEXT_PRIVATE_'))),
-      TRUSTED_ORIGINS: [process.env.NODE_ENV === 'development' && 'http://localhost:3000'].filter(Boolean),
+      TRUSTED_ORIGINS: [
+        process.env.NEXT_PRIVATE_API_URL,
+        process.env.NODE_ENV === 'development' && 'http://localhost:3000',
+      ].filter(Boolean),
     },
   },
   webpack: config => {
