da4fda312cd178458a2ef1eb3c76f365d506f517
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2024-06-18 21:08:55 +0800

    @hotfix-r1dev fix concat api

--- packages/core-library/api/calc/calc-api.ts
+++ packages/core-library/api/calc/calc-api.ts
@@ -10,29 +10,36 @@ export class CalculationApi {
   public ItemSelect(props: ItemSelectTypes) {
     const { accountId, examGroupId, shouldDisplayNextItem } = props;
     return this.axios.get<CalcItemSelectResponseItem[]>(
-      `/baseAppload/item-select-regular/${accountId}/${examGroupId}/${shouldDisplayNextItem}`
+      `/v1/api/baseAppload/item-select-regular/${accountId}/${examGroupId}/${shouldDisplayNextItem}`
     );
   }
   public ItemSelectSession(props: ItemSessionTypes) {
     const { accountId, SessionItem } = props;
     return this.axios.get<string>(
-      `/baseAppload/item-select-session/${SessionItem}/${accountId}`
+      `/v1/api/baseAppload/item-select-session/${SessionItem}/${accountId}`
     );
   }
   public DisplayNextItemSelection(accountId: string) {
     return this.axios.get<CalcItemSelectResponseItem[]>(
-      `/baseAppload/display-next-item/${accountId}`
+      `/v1/api/baseAppload/display-next-item/${accountId}`
     );
   }
   public createAnswer(props: RegularAnswer) {
-    return this.axios.post<number>("/BaseCentralized/calc-cumulatives", props);
+    return this.axios.post<number>(
+      "/v1/api/BaseCentralized/calc-cumulatives",
+      props
+    );
   }
 
   public initializeLoadPTestHimem() {
-    return this.axios.get<number>("/baseAppload/processor-load-ptest-himem");
+    return this.axios.get<number>(
+      "/v1/api/baseAppload/processor-load-ptest-himem"
+    );
   }
 
   public initializeLoadPrepareTrackItem() {
-    return this.axios.get<number>("/baseAppload/processor-prep-track-item");
+    return this.axios.get<number>(
+      "/v1/api/baseAppload/processor-prep-track-item"
+    );
   }
 }
