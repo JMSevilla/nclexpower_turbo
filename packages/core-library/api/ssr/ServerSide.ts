d155258fd66d7efe52ecf84061ea6193ed0b9374
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2024-06-18 20:40:44 +0800

    @hotfix-r1dev ft investigate error connection

--- packages/core-library/api/ssr/ServerSide.ts
+++ packages/core-library/api/ssr/ServerSide.ts
@@ -7,20 +7,4 @@ import {
 
 export class ServerSideApi {
   constructor(private readonly axios: AxiosInstance) {}
-  /**
-   *
-   * @deprecated
-   */
-  public createAnswer(props: RegularAnswer) {
-    return this.axios.post<number>(
-      "/api/answer/regular-answer/create-answer",
-      props
-    );
-  }
-  public createQuestionSession(props: ItemSessionTypes) {
-    return this.axios.post<string | null | undefined>(
-      "/api/session/question/init-session",
-      props
-    );
-  }
 }
