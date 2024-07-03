396f7b460f8612f2ec506c16dfa23fe1c3b87618
costales arnel <costalesarnel08@gmail.com>
2024-07-04 00:11:01 +0800

    @86eppzyu2/Authorize-nauthorized-Detection

--- apps/web-backoffice-generic/src/pages/shared/Layout.tsx
+++ apps/web-backoffice-generic/src/pages/shared/Layout.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider, CssBaseline, useTheme } from "@mui/material";
 import { PageContainer, LoadablePageContent } from "@/components";
 import { ControlledToast, DrawerLayout } from "core-library/components";
 import { ToastProvider } from "core-library/contexts";
+import { useAuthContext } from "core-library/contexts";
 
 interface Props {}
 
@@ -13,13 +14,18 @@ export const Layout: React.FC<React.PropsWithChildren<Props>> = ({
   const queryClient = new QueryClient();
   const theme = useTheme();
 
+  const { isAuthenticated } = useAuthContext();
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <DrawerLayout isAuthenticated={false} menu={[]}>
+        <DrawerLayout isAuthenticated={isAuthenticated} menu={[]}>
           <PageContainer stickOut={false}>
-            <LoadablePageContent loading={false}>
+            <LoadablePageContent
+              loading={false}
+              isAuthenticated={isAuthenticated}
+            >
               <ToastProvider>
                 <ControlledToast autoClose={5000} hideProgressBar={false} />
                 {children}
