ee8223106bce335ff705df77c3cbaac2451baa8a
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2024-07-07 03:18:31 +0800

    @86epq0163 #Add additional field in simulator access page for uat test purposes v3

--- apps/simulator/src/core/schema/AccessToken/validation.ts
+++ apps/simulator/src/core/schema/AccessToken/validation.ts
@@ -1,7 +1,8 @@
 import * as yup from 'yup';
 
 export const AccessKeySchema = yup.object({
-  accessKey: yup.string().required('This is Required').default(''),
+  email: yup.string().email().required('This is Required').default(''),
+  password: yup.string().required('password is required').default(''),
   appName: yup.string().default(''),
 });
 export type AccessKeyType = yup.InferType<typeof AccessKeySchema>;
