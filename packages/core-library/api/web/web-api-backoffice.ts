412c3332a90339fc8e18e0d75eaa5137001e8ee9
Jezreel Christian Bacasno <bacasnojezreel@gmail.com>
2024-07-07 01:49:04 +0800

    @86epkjffc #Query-String Installed - HomePage Header Reusable & Footer

--- packages/core-library/api/web/web-api-backoffice.ts
+++ packages/core-library/api/web/web-api-backoffice.ts
@@ -9,7 +9,6 @@ import { CmsPageResponse } from "../../types/page";
 import { CmsGlobals } from "../../types/global";
 import { CmsFooter } from "../../types/tenant";
 import { MenuItem } from "../../types/menu";
-import qs from "query-string";
 import { CategoryListResponse } from "../../types/category-response";
 import {
   CategoryFormParams,
@@ -23,7 +22,7 @@ export class WebApiBackOffice {
   constructor(
     private readonly axios: AxiosInstance,
     private readonly ssrAxios: AxiosInstance
-  ) {}
+  ) { }
   public tokenInformation() {
     /* get tokenize informations */
     return this.axios.get<CmsTokens>("");
@@ -60,8 +59,8 @@ export class WebApiBackOffice {
       return await this.axios.get<CmsGlobals>(
         contentAccessKey
           ? `/api/content-api/api/v2/content/authorized-globals?${qs.stringify({
-              contentAccessKey: "",
-            })}`
+            contentAccessKey: "",
+          })}`
           : `/api/v2/content/BaseContent/unauthorized-globals?${qs.stringify({ tenantUrl })}`,
         { headers: { ENV: "dev2" } }
       );
