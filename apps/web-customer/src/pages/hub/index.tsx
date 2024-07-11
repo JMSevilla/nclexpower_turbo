ccc7955f94d5724a30d31a7ba11e0e52273b521d
Jezreel Christian Bacasno <bacasnojezreel@gmail.com>
2024-07-11 11:21:37 +0800

    #86epkyztu @Props Preparation in Hub - Placed dashboard in hub folder

--- apps/web-customer/src/pages/hub/index.tsx
+++ apps/web-customer/src/pages/hub/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 interface Props { }
 
-const WebCustomerHub: React.FC<Props> = (props) => {
+const HubOverview: React.FC<Props> = (props) => {
     return (
         <div className='flex grow bg-slate-500 items-center justify-center h-screen'>
             Web Customer Hub
@@ -10,4 +10,4 @@ const WebCustomerHub: React.FC<Props> = (props) => {
     )
 }
 
-export default WebCustomerHub
+export default HubOverview
