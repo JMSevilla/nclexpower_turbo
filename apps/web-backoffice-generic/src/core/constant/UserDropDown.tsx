13fc8bf308d25421abf539a30169b5bf59267655
Jezreel Christian Bacasno <bacasnojezreel@gmail.com>
2024-07-15 22:57:17 +0800

    @86epu3gf5 #OnClick Issue Addressed - Relocation of Logout

--- apps/web-backoffice-generic/src/core/constant/UserDropDown.tsx
+++ apps/web-backoffice-generic/src/core/constant/UserDropDown.tsx
@@ -1,13 +1,14 @@
 import { Logout as LogoutIcon } from "@mui/icons-material";
 import { NavigationType } from 'core-library/types/navigation';
 
+const handleLogout = () => { }
+
 export const DropDownMenu: NavigationType[] = [
     {
         id: 1,
         label: "Logout",
         icon: <LogoutIcon fontSize="small" />,
-        onClick: () => { console.log("Logout Clicked") }
-
+        onClick: handleLogout
     },
 ]
 
