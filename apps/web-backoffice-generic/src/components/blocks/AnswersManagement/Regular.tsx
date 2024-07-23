c6742730985c4e812792e29bb4acbc9efe0ad6be
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2024-07-23 13:11:36 +0800

    @86epvyx8a #High-level implementation stepper

--- apps/web-backoffice-generic/src/components/blocks/AnswersManagement/Regular.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const RegAnswers = () => {
-  return (
-    <div>
-      <h1>This is regular question Answer</h1>
-    </div>
-  );
-};
--- apps/web-backoffice-generic/src/components/blocks/QuestionnaireManagement/CaseStudy.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const CSQuestionnaire = () => {
-  return (
-    <div>
-      <h1>This is CaseStudy Questionnaire</h1>
-    </div>
-  );
-};
