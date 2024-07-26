58b8bb328dab2b8b357f13954c7a4f836e0d414d
Lizter <lizters74@gmail.com>
2024-07-26 23:35:22 +0800

    @86epxbaq6 #404 Page not found Ui

--- apps/web-customer/src/components/blocks/NotFoundBlock/NotFoundBlock.tsx
+++ apps/web-customer/src/components/blocks/NotFoundBlock/NotFoundBlock.tsx
@@ -5,18 +5,26 @@ import { useRouter } from "next/router";
 export const NotfoundBlock: React.FC = () => {
   const router = useRouter();
 
-  const BackToHome = () => {
-    router.push("/");
+  const Back = () => {
+    router.back();
   };
 
   return (
-    <div className="h-screen flex items-center justify-center flex-col font-['Poppins']">
-      <Image className="w-[700px]" src={PageNotFound} alt="ImageOne" />
+    <div className="h-screen flex items-center justify-center flex-col font-['Poppins'] text-center p-4">
+      <Image
+        className="w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl"
+        src={PageNotFound}
+        alt="Page Not Found"
+      />
+      <p className="mt-4 text-lg md:text-xl lg:text-2xl">
+        The page you were looking for: <strong>{router.asPath}</strong> could
+        not be found.
+      </p>
       <button
-        className="text-white bg-[#2557D6] hover:bg-[#2557D6]/90 focus:ring-4 focus:ring-[#2557D6]/50 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#2557D6]/50 me-2 mb-2"
-        onClick={BackToHome}
+        className="mt-4 text-white bg-[#2557D6] hover:bg-[#2557D6]/90 focus:ring-4 focus:ring-[#2557D6]/50 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#2557D6]/50"
+        onClick={Back}
       >
-        BACK TO HOMEPAGE
+        BACK
       </button>
     </div>
   );
