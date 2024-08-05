358ca39c6fb4a419e2fe101349bef59ac1047f6b
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2024-08-05 17:17:20 +0800

    @hotfix_1 #contact form

--- apps/web-customer/src/components/blocks/ContactBlock/ContactForm.tsx
+++ apps/web-customer/src/components/blocks/ContactBlock/ContactForm.tsx
@@ -1,35 +1,52 @@
-import React from 'react';
+import React from "react";
 import { Box, Button, Grid, Typography } from "@mui/material";
 import { TextField } from "core-library/components";
-import { ContactFormType } from '../../../core/Schema';
-import { Control, UseFormHandleSubmit } from 'react-hook-form';
+import { ContactFormType } from "@/core/Schema";
+import { Control, UseFormHandleSubmit } from "react-hook-form";
 
 interface FormValues {
-  control: Control<ContactFormType>
-  handleSubmit: UseFormHandleSubmit<ContactFormType>
+  control: Control<ContactFormType>;
+  handleSubmit: UseFormHandleSubmit<ContactFormType>;
   onSubmit: (data: ContactFormType) => void;
 }
 
-export const ContactForm: React.FC<FormValues> = ({ control, handleSubmit, onSubmit }) => {
+export const ContactForm: React.FC<FormValues> = ({
+  control,
+  handleSubmit,
+  onSubmit,
+}) => {
   return (
     <section className="w-full h-screen flex items-center justify-center">
-      <div className="absolute w-full h-screen z-0 bg-pricing ">
-      </div>
-      <Grid container sx={{ width: '60%', zIndex: 1 }}>
+      <div className="absolute w-full h-screen z-0 bg-pricing "></div>
+      <Grid container sx={{ width: "60%", zIndex: 1 }}>
         <Grid item xl={12} sx={{ p: { xs: 2, sm: 4, lg: 6, xl: 8 } }}>
           <div className="flex items-center p-12 justify-center flex-col bg-white border rounded-md">
             <div className="text-center mb-5">
-              <Typography variant="h3" component="span" sx={{ color: '#007AB7', fontFamily: 'Poppins', fontWeight: 'bold' }}>
+              <Typography
+                variant="h3"
+                component="span"
+                sx={{
+                  color: "#007AB7",
+                  fontFamily: "Poppins",
+                  fontWeight: "bold",
+                }}
+              >
                 Connect with us
               </Typography>
-              <p>
-                The NCLEX team would love to hear from you.
-              </p>
+              <p>The NCLEX team would love to hear from you.</p>
             </div>
-            <form onSubmit={handleSubmit(onSubmit)} className='w-full'>
-              <Grid item lg={12} sx={{ marginY: 2, display: 'flex', gap: 1 }}>
-                <TextField name="firstName" control={control} label="First Name" />
-                <TextField name="lastName" control={control} label="Last Name" />
+            <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+              <Grid item lg={12} sx={{ marginY: 2, display: "flex", gap: 1 }}>
+                <TextField
+                  name="firstName"
+                  control={control}
+                  label="First Name"
+                />
+                <TextField
+                  name="lastName"
+                  control={control}
+                  label="Last Name"
+                />
               </Grid>
               <Grid item lg={12} sx={{ marginY: 2 }}>
                 <TextField name="email" control={control} label="Email" />
@@ -44,8 +61,21 @@ export const ContactForm: React.FC<FormValues> = ({ control, handleSubmit, onSub
                   placeholder="Kindly enter your message here..."
                 />
               </Grid>
-              <Box sx={{ gridColumn: 'span 10', display: 'flex', alignItems: 'center', gap: 4 }}>
-                <Button type="submit" variant="contained" fullWidth color="primary" sx={{ px: 4, py: 2 }}>
+              <Box
+                sx={{
+                  gridColumn: "span 10",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 4,
+                }}
+              >
+                <Button
+                  type="submit"
+                  variant="contained"
+                  fullWidth
+                  color="primary"
+                  sx={{ px: 4, py: 2 }}
+                >
                   Submit
                 </Button>
               </Box>
@@ -53,6 +83,6 @@ export const ContactForm: React.FC<FormValues> = ({ control, handleSubmit, onSub
           </div>
         </Grid>
       </Grid>
-    </section >
+    </section>
   );
 };
