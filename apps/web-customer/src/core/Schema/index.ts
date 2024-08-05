555f3a4dda4e457bf1d78ff5c0bed197c5944238
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2024-08-05 19:13:07 +0800

    @hotfix_7 #contact form

--- apps/web-customer/src/core/Schema/index.ts
+++ apps/web-customer/src/core/Schema/index.ts
@@ -1,4 +1,3 @@
-export * from "./ContactForm/validation";
 export * from "./ReportIssueForm/validation";
 export * from "./ForgotPasswordForm/validation";
 export * from "./ReportIssueForm/validation";
