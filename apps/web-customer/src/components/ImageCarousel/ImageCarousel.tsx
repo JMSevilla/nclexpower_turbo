1cb31ed79c5847754d380c3dddee7d50efcead46
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2024-08-05 19:27:32 +0800

    @hotfix_9 #contact form

--- apps/web-customer/src/components/ImageCarousel/ImageCarousel.tsx
+++ apps/web-customer/src/components/ImageCarousel/ImageCarousel.tsx
@@ -1,12 +1,12 @@
-import { SliderConfigType } from 'core-library/types/global';
-import Slider from 'react-slick';
+import { SliderConfigType } from "core-library/types/global";
+import Slider from "react-slick";
 
-export const ImageCarousel: React.FC<React.PropsWithChildren<SliderConfigType>> = ({ sliderConfig, children }) => {
-    return (
-        <Slider {...sliderConfig} className='h-full object-none'>
-            {children}
-        </Slider>
-    )
-}
-
-
+export const ImageCarousel: React.FC<
+  React.PropsWithChildren<SliderConfigType>
+> = ({ sliderConfig, children }) => {
+  return (
+    <Slider {...sliderConfig} className="h-full object-none">
+      {children}
+    </Slider>
+  );
+};
