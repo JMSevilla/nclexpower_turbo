f21801e0af9fd57f88edc4313220ae889dbb3255
Jacob allen <vjacoballen3040@gmail.com>
2024-08-14 20:32:48 +0800

    @86epxqy15 #Remove second return inside a component

--- apps/web-backoffice-generic/src/components/RichTextEditor/EditorButtonGroup.tsx
+++ apps/web-backoffice-generic/src/components/RichTextEditor/EditorButtonGroup.tsx
@@ -6,19 +6,17 @@ import React from 'react';
 
 
 export const EditorButtonGroup = ({ menus }: { menus: MenuButtonType[] }) => {
-    return menus.map((button, index) => {
-        return (
-            <Box key={index} sx={{ backgroundColor: button.isActive ? 'purple' : '' }} borderRadius='5px'  >
-                <IconButton sx={{
-                    color: button.isActive ? 'white' : '#333',
-                    height: '30px',
-                    fontSize: '12px',
-                }}
-                    onClick={button.onClick}
-                    disabled={button.disabled} >
-                    {button.icon ?? button.label}
-                </IconButton>
-            </Box>
-        )
-    })
+    return menus.map((button, index) =>
+        <Box key={index} sx={{ backgroundColor: button.isActive ? 'purple' : '' }} borderRadius='5px'  >
+            <IconButton sx={{
+                color: button.isActive ? 'white' : '#333',
+                height: '30px',
+                fontSize: '12px',
+            }}
+                onClick={button.onClick}
+                disabled={button.disabled} >
+                {button.icon ?? button.label}
+            </IconButton>
+        </Box>
+    )
 }
