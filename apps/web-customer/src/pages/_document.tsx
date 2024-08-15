3435148dbe78620aff9c585f34c8cc0f50cd491d
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2024-08-15 22:27:44 +0800

    @86epy59jk #Content Security Policy WC

--- apps/web-customer/src/pages/_document.tsx
+++ apps/web-customer/src/pages/_document.tsx
@@ -1,13 +1,55 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import { nonce } from "core-library/types";
+import { config } from "core-library/config";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+type MyDocumentProps = {
+  generatedNonce: string;
+};
+
+export default class MyDocument extends Document<MyDocumentProps> {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const generatedNonce = nonce();
+    const isDevelopment = process.env.NODE_ENV === "development";
+
+    const csp =
+      `default-src 'self' *.vercel.app; script-src 'self' 'nonce-${generatedNonce}' *.vercel.app *.herokuapp.com ` +
+      `; form-action 'self'; base-uri 'self'; object-src 'self'; style-src 'self' 'unsafe-inline'; connect-src ` +
+      config.value.API_URL +
+      " " +
+      config.value.VERCELURL +
+      " *.vercel.app *.herokuapp.com " +
+      `blob:; img-src 'self' data:; font-src 'self' data:; frame-src 'self' *.vercel.app;`;
+    const res = ctx?.res;
+    if (res != null && !isDevelopment && !res.headersSent) {
+      res.setHeader("Content-Security-Policy", csp);
+    }
+
+    return {
+      ...initialProps,
+      generatedNonce,
+    };
+  }
+
+  render() {
+    const { generatedNonce } = this.props;
+    return (
+      <Html lang="en">
+        <Head nonce={generatedNonce}>
+          <meta charSet="utf-8" />
+          <meta name="csp-nonce" content={generatedNonce} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript nonce={generatedNonce} />
+        </body>
+      </Html>
+    );
+  }
 }
