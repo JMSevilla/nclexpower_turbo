a40bb801cfe14e67f29d808e2a95bdc4de1290f5
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2024-08-17 02:46:58 +0800

    @86epy59jk #Server side CSP

--- apps/web-customer/src/pages/_document.tsx
+++ apps/web-customer/src/pages/_document.tsx
@@ -1,55 +1,13 @@
-import Document, {
-  DocumentContext,
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from "next/document";
-import { nonce } from "core-library/types";
-import { config } from "core-library/config";
+import { Html, Head, Main, NextScript } from "next/document";
 
-type MyDocumentProps = {
-  generatedNonce: string;
-};
-
-export default class MyDocument extends Document<MyDocumentProps> {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    const generatedNonce = nonce();
-    const isDevelopment = process.env.NODE_ENV === "development";
-
-    const csp =
-      `default-src 'self' *.vercel.app; script-src 'self' 'nonce-${generatedNonce}' *.vercel.app *.herokuapp.com ` +
-      `; form-action 'self'; base-uri 'self'; object-src 'self'; style-src 'self' 'unsafe-inline'; connect-src ` +
-      config.value.API_URL +
-      " " +
-      config.value.VERCELURL +
-      " *.vercel.app *.herokuapp.com " +
-      `blob:; img-src 'self' data:; font-src 'self' data:; frame-src 'self' *.vercel.app;`;
-    const res = ctx?.res;
-    if (res != null && !isDevelopment && !res.headersSent) {
-      res.setHeader("Content-Security-Policy", csp);
-    }
-
-    return {
-      ...initialProps,
-      generatedNonce,
-    };
-  }
-
-  render() {
-    const { generatedNonce } = this.props;
-    return (
-      <Html lang="en">
-        <Head nonce={generatedNonce}>
-          <meta charSet="utf-8" />
-          <meta name="csp-nonce" content={generatedNonce} />
-        </Head>
-        <body>
-          <Main />
-          <NextScript nonce={generatedNonce} />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
--- apps/web-customer/src/pages/index.tsx
+++ apps/web-customer/src/pages/index.tsx
@@ -4,22 +4,37 @@ import {
   HowItWorksBlock,
   PricingBlock,
 } from "@/components";
+import { withCSP } from "core-library";
+import CSPHead from "core-library/components/CSPHead";
+import { GetServerSideProps } from "next";
+import React from "react";
 
-export default function Home() {
-  return (
-    <div className="w-screen flex flex-col overflow-y-auto overflow-x-hidden font-ptSans ">
-      <div className="w-full h-screen">
-        <RevolutionBannerBlock />
-      </div>
-      <div className="w-full h-fit">
-        <CoreZigmaBlock />
-      </div>
-      <div className="w-full h-fit">
-        <HowItWorksBlock />
-      </div>
-      <div className="w-full h-fit" id="pricing" >
-        <PricingBlock />
-      </div>
-    </div>
-  );
+interface Props {
+  generatedNonce: string;
 }
+
+const Home: React.FC<Props> = ({ generatedNonce }) => {
+  return (
+    <React.Fragment>
+      <CSPHead nonce={generatedNonce} />
+      <div className="w-screen flex flex-col overflow-y-auto overflow-x-hidden font-ptSans ">
+        <div className="w-full h-screen">
+          <RevolutionBannerBlock />
+        </div>
+        <div className="w-full h-fit">
+          <CoreZigmaBlock />
+        </div>
+        <div className="w-full h-fit">
+          <HowItWorksBlock />
+        </div>
+        <div className="w-full h-fit" id="pricing">
+          <PricingBlock />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = withCSP();
+
+export default Home;
