7c1f143394566e27570452a653e3abf98ecedad0
bryan <bry@payloro.com>
2024-08-22 21:29:10 +0800

    @86epydjx3 #Revamp Contact Us Page update

--- apps/web-customer/src/components/blocks/ContactBlock/ContactHero.tsx
+++ apps/web-customer/src/components/blocks/ContactBlock/ContactHero.tsx
@@ -1,12 +1,12 @@
 import { Box } from "@mui/material";
-import ContactHeroBanner from "../../../assets/contact/ContactUsBanner.png";
+import { ContactUsBanner } from "core-library/assets";
 import Image from "next/image";
 
 export const ContactHero: React.FC = () => {
   return (
         <Box className="h-auto" position="relative">
           <div className="relative h-[633px]">
-            <Image className='w-full h-[633px] relative object-cover' loading='lazy' src={ContactHeroBanner} alt="ContactUsBanner" />
+            <Image className='w-full h-[633px] relative object-cover' loading='lazy' src={ContactUsBanner} alt="ContactUsBanner" />
             <div className="absolute inset-0 z-10 flex flex-col items-center justify-center space-y-4 lg:items-start lg:justify-start lg:ml-[12%] lg:mt-[12%]">
               <p className="lg:text-6xl text-4xl font-ptSans text-yellow font-bold text-center lg:text-left">
                 Get In Touch
