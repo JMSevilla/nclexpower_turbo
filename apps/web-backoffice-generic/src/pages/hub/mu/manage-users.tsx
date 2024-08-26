1dc894f4b4accdc1477ff1005a197c693825024d
Brian James <brianjamesdlcz@gmail.com>
2024-08-26 15:05:52 +0800

    @86epq04wa #Fixed error on file name

--- apps/web-backoffice-generic/src/pages/hub/mu/manage-users.tsx
+++ apps/web-backoffice-generic/src/pages/hub/mu/manage-users.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import withAuth from "core-library/core/utils/withAuth";
-import InternalUsersBlock from "@/components/blocks/internal-users/internal-users-block";
+import InternalUsersBlock from "@/components/blocks/internal-users/InternalUsersBlock";
 
 const ManageUserPage: React.FC = () => {
   return <InternalUsersBlock />;
