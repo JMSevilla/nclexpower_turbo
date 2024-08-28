497a27c14c395851bb407c26ddc7d4f2ee27c9fc
costales arnel <costalesarnel08@gmail.com>
2024-08-28 22:33:57 +0800

    @86epxqymx #Reusable-accordion-integration-with-mapping-from-atom

---
+++ packages/core-library/tests/page/SummaryAccordion.test.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { fireEvent, render, screen } from "core-library/tests/common";
+import "@testing-library/jest-dom";
+import { SummaryAccordion } from "../../components";
+
+// Mocking necessary dependencies
+jest.mock("../../config", () => ({
+  config: { value: jest.fn() },
+}));
+
+jest.mock("../../core/router", () => ({
+  useRouter: jest.fn(),
+}));
+
+jest.mock("../../hooks", () => ({
+  useSanitizedInputs: () => ({
+    purifyInputs: (input: string) => input,
+  }),
+}));
+
+describe("SummaryAccordion Component", () => {
+  const mockItem = {
+    question: "What is the capital of France?",
+    clientNeeds: "Geography",
+    cognitiveLevel: "Recall",
+    contentArea: "World Knowledge",
+    answers: [
+      { answer: "Paris", answerKey: true },
+      { answer: "London", answerKey: false },
+    ],
+  };
+
+  const type = "MCQ";
+  const index = 0;
+
+  it("renders the SummaryAccordion component", () => {
+    render(<SummaryAccordion item={mockItem} type={type} index={index} />);
+    expect(
+      screen.getByText(/What is the capital of France\?/i)
+    ).toBeInTheDocument();
+  });
+
+  it("shows the AnswerOptionSummary component when question is clicked", () => {
+    render(<SummaryAccordion item={mockItem} type={type} index={index} />);
+
+    fireEvent.click(screen.getByText(mockItem.question));
+
+    expect(screen.getByText(mockItem.clientNeeds)).toBeVisible();
+    expect(screen.getByText(mockItem.cognitiveLevel)).toBeVisible();
+    expect(screen.getByText(mockItem.contentArea)).toBeVisible();
+    mockItem.answers.forEach((answer) => {
+      expect(screen.getByText(answer.answer)).toBeVisible();
+    });
+  });
+});
