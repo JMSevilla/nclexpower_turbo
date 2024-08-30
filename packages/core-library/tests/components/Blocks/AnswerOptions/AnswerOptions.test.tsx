a6c83ca87961697cd5c9631d5a5220e595ef50e6
Jacob allen <vjacoballen3040@gmail.com>
2024-08-31 03:35:14 +0800

    @86epxqyup #Remove spaces and transistion to absolute imports

--- packages/core-library/tests/components/Blocks/AnswerOptions/AnswerOptions.test.tsx
+++ packages/core-library/tests/components/Blocks/AnswerOptions/AnswerOptions.test.tsx
@@ -28,7 +28,6 @@ describe('AnswerOptions', () => {
         expect(screen.getByTestId('mcq-answer')).toBeInTheDocument()
     })
 
-
     it('should render the SATA answer options', () => {
         render(<FormProvider {...form}><AnswerOptions {...DEFAULT_PROPS} questionnaireType='SATA' /></FormProvider>)
         expect(screen.getByTestId('sata-answer')).toBeInTheDocument()
@@ -38,7 +37,6 @@ describe('AnswerOptions', () => {
         const { container } = render(<FormProvider {...form}><AnswerOptions questionIndex={1} questionType='caseStudy' questionnaireType='SATA' /></FormProvider>)
         expect(container).toBeEmptyDOMElement()
     })
-
 
     it('should render nothing when questionType is caseStudy and MCQ', () => {
         const { container } = render(<FormProvider {...form}><AnswerOptions questionIndex={1} questionType='caseStudy' questionnaireType='MCQ' /></FormProvider>)
