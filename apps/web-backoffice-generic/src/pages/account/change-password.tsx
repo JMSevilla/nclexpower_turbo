77849712db568c0159701a935f0c4031c2d7e079
bryan <bry@payloro.com>
2024-08-30 11:30:20 +0800

    @86eq2zzue #Added Forgot and Change Password UI with Mock Questions

---
+++ apps/web-backoffice-generic/src/pages/account/change-password.tsx
@@ -0,0 +1,7 @@
+import { ChangePasswordBlock } from "../../components/blocks/page/ChangePassword/ChangePasswordBlock";
+
+function ChangePasswordPage() {
+  return <ChangePasswordBlock />;
+}
+
+export default ChangePasswordPage;
