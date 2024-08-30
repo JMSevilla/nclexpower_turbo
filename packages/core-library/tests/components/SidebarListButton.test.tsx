5796f1df31ca3b50e95aaf92e6794c69e2f5ab51
JezChristian <bacasnojezreel@gmail.com>
2024-08-31 03:04:30 +0800

    @86eq44vt #Unit Testing SidebarListButton

---
+++ packages/core-library/tests/components/SidebarListButton.test.tsx
@@ -0,0 +1,41 @@
+import { render } from '@testing-library/react';
+import { screen } from '../common';
+import { SidebarListButton } from '../../components/GenericSidebar/SidebarListButton';
+
+jest.mock("../../config", () => ({
+    config: { value: jest.fn() },
+}));
+
+jest.mock("../../core/router", () => ({
+    useRouter: jest.fn(),
+}));
+
+const mockNavigation = {
+    id:0,
+    path: '/',
+    label: 'Home',
+    icon: <span>Icon</span>,
+    children: [
+        {
+        id:0,
+        path: '/child',
+        label: 'Child',
+        icon: <span>Child Icon</span>,
+        children: [],
+        },
+    ],
+};
+
+describe('SidebarListButton', () => {
+  it('should render the component with the provided props', () => {
+    render(<SidebarListButton navigation={mockNavigation} pathname="/" />);
+
+    expect(screen.getByText('Home')).toBeInTheDocument();
+    expect(screen.getByText('Icon')).toBeInTheDocument();
+  });
+
+  it('should not render children if `open` is false', () => {
+    render(<SidebarListButton navigation={{ ...mockNavigation, children: [] }} pathname="/" />);
+    expect(screen.queryByText('Child')).toBeNull();
+  });
+});
