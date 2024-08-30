1a5217f5781f72497bb842751aad75c246747b81
Jacob allen <vjacoballen3040@gmail.com>
2024-08-30 21:56:03 +0800

    @86epxqyup #Cover test for AnswerOptions and fix datatypes in answer options

---
+++ packages/core-library/tests/components/Blocks/AnswerOptions/AnswerOptions.test.tsx
@@ -0,0 +1,48 @@
+import { FormProvider, useForm } from 'react-hook-form';
+import { AnswerOptions, AnswerOptionsType } from '../../../../components'
+import { render, renderHook, screen } from '../../../common'
+
+jest.mock("../../../../config", () => ({
+    getConfig: jest
+        .fn()
+        .mockReturnValue({ publicRuntimeConfig: { processEnv: {} } }),
+    config: { value: jest.fn() },
+}));
+
+jest.mock("../../../../core/router", () => ({
+    useRouter: jest.fn(),
+}));
+
+const DEFAULT_PROPS: AnswerOptionsType = {
+    questionIndex: 1,
+    questionType: 'regularQuestion',
+    questionnaireType: 'MCQ'
+}
+
+describe('AnswerOptions', () => {
+    const { result } = renderHook(() => useForm())
+    const form = result.current
+
+    it('should render the mcq answer options', () => {
+        render(<FormProvider {...form}><AnswerOptions {...DEFAULT_PROPS} questionnaireType='MCQ' /></FormProvider>)
+        expect(screen.getByTestId('mcq-answer')).toBeInTheDocument()
+    })
+
+
+    it('should render the SATA answer options', () => {
+        render(<FormProvider {...form}><AnswerOptions {...DEFAULT_PROPS} questionnaireType='SATA' /></FormProvider>)
+        expect(screen.getByTestId('sata-answer')).toBeInTheDocument()
+    })
+
+    it('should render nothing ', () => {
+        const { container } = render(<FormProvider {...form}><AnswerOptions questionIndex={1} questionType='caseStudy' questionnaireType='SATA' /></FormProvider>)
+        expect(container).toBeEmptyDOMElement()
+    })
+
+
+    it('should render nothing when questionType is caseStudy and MCQ', () => {
+        const { container } = render(<FormProvider {...form}><AnswerOptions questionIndex={1} questionType='caseStudy' questionnaireType='MCQ' /></FormProvider>)
+        expect(container).toBeEmptyDOMElement()
+    })
+
+})
