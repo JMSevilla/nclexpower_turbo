55a6c04829c6e6b6a95471d7bd90193e3775b676
JezChristian <bacasnojezreel@gmail.com>
2024-08-30 23:51:32 +0800

    @86eq44vt #Unit Testing usePageLoader

--- packages/core-library/hooks/usePageLoader.ts
+++ packages/core-library/hooks/usePageLoader.ts
@@ -22,7 +22,6 @@ export const usePageLoader = (): PageLoaderReturnType => {
     useEffect(() => {
         router.events.on("routeChangeStart", handleRouteChangeStart);
         router.events.on("routeChangeComplete", handleRouteChangeComplete);
-
         return () => {
             router.events.off("routeChangeStart", handleRouteChangeStart);
             router.events.off("routeChangeComplete", handleRouteChangeComplete);
