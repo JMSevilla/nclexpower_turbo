8231dbfb109a5b8e515c78582755830228b388f2
bryan <bry@payloro.com>
2024-08-30 11:30:20 +0800

    @86eq2zzue #Added Forgot and Change Password UI with Mock Questions

---
+++ apps/web-backoffice-generic/src/components/blocks/page/ForgotPassword/ForgotPasswordForm.tsx
@@ -0,0 +1,122 @@
+import { Box } from "@mui/material";
+import {
+  forgotPasswordSchema,
+  ForgotPasswordType,
+} from "../../../../core/schema/ForgotPasswordForm/validation";
+import { Alert, TextField } from "core-library/components";
+import { FormProvider, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button } from "core-library/components";
+import Link from "next/link";
+import Lottie from "lottie-react";
+import { ForgotPasswordImage } from "core-library/assets";
+
+interface Props {
+  onSubmit: (values: ForgotPasswordType) => void;
+  submitLoading?: boolean;
+  showAlert?: boolean;
+  isExpired?: boolean;
+}
+
+export const ForgotPasswordForm: React.FC<Props> = ({
+  onSubmit,
+  submitLoading,
+  showAlert,
+  isExpired,
+}) => {
+  const form = useForm({
+    mode: "onSubmit",
+    resolver: yupResolver(forgotPasswordSchema),
+    defaultValues: forgotPasswordSchema.getDefault(),
+  });
+
+  const {
+    control,
+    handleSubmit,
+    formState: { isValid },
+  } = form;
+
+  return (
+    <section className="w-full h-full flex justify-center items-center">
+      <Box className="container-sm flex flex-col lg:flex-row items-center justify-center w-full space-x-0 px-8 lg:space-x-14 lg:px-4">
+        <div className="flex">
+          <Lottie
+            animationData={ForgotPasswordImage}
+            style={{ width: 280, height: 280 }}
+          />
+        </div>
+
+        <Box className="flex flex-col">
+          <Box className="flex flex-col text-center mt-4 mb-4">
+            <p className="leading-none uppercase text-xl font-semibold lg:text-2xl">
+              Forgot your
+              <span className="ml-1 text-blue">password?</span>
+            </p>
+
+            <p className="leading-none text-[14px] lg:text-[16px] text-curveGray">
+              Enter your registered email, we'll help you reset your password.
+            </p>
+          </Box>
+
+          <FormProvider {...form}>
+            <TextField
+              control={control}
+              placeholder="Email Address"
+              name="email"
+              sx={{
+                borderRadius: "10px",
+                width: "100%",
+              }}
+              inputProps={{ style: { padding: 15, borderRadius: "10px" } }}
+            />
+            {showAlert && (
+              <div className="pt-2">
+                <Alert
+                  severity="success"
+                  title="Successfully sent to your email"
+                />
+              </div>
+            )}
+            {isExpired && (
+              <div className="pt-2">
+                <Alert
+                  severity="error"
+                  title="Account Expired"
+                  description="The account you are trying to access is already expired."
+                />
+              </div>
+            )}
+            <div className="mt-5">
+              <Button
+                variant="contained"
+                fullWidth
+                sx={{
+                  px: 4,
+                  py: 2,
+                  borderRadius: "10px",
+                  backgroundColor: "#0F2A71",
+                }}
+                className="hover:bg-hoverBlue"
+                loading={submitLoading}
+                onClick={handleSubmit(onSubmit)}
+                disabled={!isValid || submitLoading}
+              >
+                Continue
+              </Button>
+            </div>
+          </FormProvider>
+
+          <Box className="flex items-center justify-center w-full gap-2 mt-2">
+            <p className="text-[16px]">Already know your password?</p>
+            <Link
+              href="/login"
+              className="text-[16px] underline font-semibold text-blue"
+            >
+              Sign In
+            </Link>
+          </Box>
+        </Box>
+      </Box>
+    </section>
+  );
+};
