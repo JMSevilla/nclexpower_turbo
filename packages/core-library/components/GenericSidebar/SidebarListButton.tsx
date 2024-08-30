523ad1d6917c514353289ac4498f664ccbc37d19
JezChristian <bacasnojezreel@gmail.com>
2024-08-30 23:05:10 +0800

    @86eq11bp3 #Page Loader Web Customer

--- packages/core-library/components/GenericSidebar/SidebarListButton.tsx
+++ packages/core-library/components/GenericSidebar/SidebarListButton.tsx
@@ -10,6 +10,7 @@ import { NavigationType } from "../../types/navigation";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { Divider } from "@mui/material";
 import { SidebarButton } from "./SidebarButton";
+import { useRouter } from "../../core";
 
 type SidebarListButtonProps = {
   navigation: NavigationType;
@@ -21,6 +22,8 @@ export const SidebarListButton = ({
   pathname,
 }: SidebarListButtonProps) => {
   const [open, setOpen] = useState<boolean>(true);
+  const router = useRouter();
+  const path = router.pathname
 
   const handleCollapseButton = () => {
     setOpen((prev) => !prev);
@@ -30,7 +33,7 @@ export const SidebarListButton = ({
     <Box width="100%">
       <Box padding={1}>
         <Box overflow="hidden" borderRadius={3}>
-          <ListItemButton onClick={handleCollapseButton}>
+          <ListItemButton  disabled={navigation.path == path} onClick={handleCollapseButton}>
             <ListItemIcon> {navigation.icon && navigation.icon}</ListItemIcon>
             <ListItemText>
               <Typography variant="body2" fontWeight={600} fontSize={13}>
