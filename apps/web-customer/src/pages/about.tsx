8bac12d62e8942fc8f9150f1b199077a2119bf8e
JezChristian <bacasnojezreel@gmail.com>
2024-08-31 22:18:30 +0800

    @86eq38yjm #CSP Implementation in WC Pages

--- apps/web-customer/src/pages/about.tsx
+++ apps/web-customer/src/pages/about.tsx
@@ -1,8 +1,22 @@
 import { AboutUsBlock } from '@/components/blocks/AboutUsBlock/AboutUsBlock'
+import { withCSP } from 'core-library';
+import CSPHead from 'core-library/components/CSPHead';
+import { GetServerSideProps } from 'next';
 import React from 'react'
 
-const AboutUs = () => {
-  return <AboutUsBlock/>
+interface Props {
+  generatedNonce: string;
 }
 
-export default AboutUs
+const AboutUs:React.FC<Props> = ({generatedNonce}) => {
+  return (
+  <>
+    <CSPHead nonce={generatedNonce} />
+    <AboutUsBlock/>
+  </>
+)
+}
+
+export const getServerSideProps: GetServerSideProps = withCSP();
+
+export default AboutUs;
