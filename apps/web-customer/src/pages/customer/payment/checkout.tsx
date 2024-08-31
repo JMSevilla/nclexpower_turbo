a97d93e3f49bd39dec609e73d6a34363d7b05bd4
JezChristian <bacasnojezreel@gmail.com>
2024-08-31 22:18:30 +0800

    @86eq38yjm #CSP Implementation in WC Pages

--- apps/web-customer/src/pages/customer/payment/checkout.tsx
+++ apps/web-customer/src/pages/customer/payment/checkout.tsx
@@ -1,6 +1,21 @@
 import React from 'react'
 import OrderCheckout from '@/components/blocks/OrderCheckoutBlock/OrderCheckoutBlock'
+import CSPHead from 'core-library/components/CSPHead';
+import { GetServerSideProps } from 'next';
+import { withCSP } from 'core-library';
 
-export default function OrderCheckoutPage() {
-    return (<OrderCheckout />)
+interface Props {
+    generatedNonce: string;
 }
+
+const OrderCheckoutPage:React.FC<Props> = ({generatedNonce}) => {
+    return (
+    <>
+    <CSPHead nonce={generatedNonce} />
+    <OrderCheckout />
+    </>
+)}
+
+export const getServerSideProps: GetServerSideProps = withCSP();
+
+export default OrderCheckoutPage
