9075afa386f0c707824a42885c8917e3ea1d3418
JezChristian <bacasnojezreel@gmail.com>
2024-08-31 22:18:30 +0800

    @86eq38yjm #CSP Implementation in WC Pages

--- apps/web-customer/src/pages/account/reset-password.tsx
+++ apps/web-customer/src/pages/account/reset-password.tsx
@@ -4,19 +4,25 @@ import {
   ResetPasswordParams,
   ValidateResetLinkTokenParams,
 } from "core-library/api/types";
-import { useRouter } from "core-library";
+import { useRouter, withCSP } from "core-library";
 import { NotFoundBlock } from "@/components/blocks/NotFoundBlock/NotFoundBlock";
 import { useEffect, useState } from "react";
 import { PageLoader } from "core-library/components";
 import { useExecuteToast } from "core-library/contexts";
 import { ChangePasswordType } from "@/core/Schema";
+import CSPHead from "core-library/components/CSPHead";
+import { GetServerSideProps } from "next";
 
 interface QueryParams {
   uId?: string;
   auth?: string;
 }
 
-export function ResetPasswordPage() {
+interface Props{
+  generatedNonce: string;
+}
+
+const ResetPasswordPage:React.FC<Props> = ({generatedNonce}) => {
   const resetPasswordCb = useApiCallback(
     async (api, args: ResetPasswordParams) =>
       await api.web.web_reset_password(args)
@@ -54,7 +60,12 @@ export function ResetPasswordPage() {
 
   if (validateCb.loading || !authorized) return <PageLoader />;
 
-  return <ChangePasswordBlock onSubmit={handleSubmit} />;
+  return (
+    <>
+    <CSPHead nonce={generatedNonce} />
+  <ChangePasswordBlock onSubmit={handleSubmit} />
+  </>
+  );
 
   async function handleSubmit(values: ChangePasswordType) {
     try {
@@ -101,5 +112,7 @@ export function ResetPasswordPage() {
     }
   }
 }
+
+export const getServerSideProps: GetServerSideProps = withCSP();
 
 export default ResetPasswordPage;
