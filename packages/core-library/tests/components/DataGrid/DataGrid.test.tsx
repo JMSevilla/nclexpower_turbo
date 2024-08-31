ea4bba657f683d874b20ef425353040eda210435
Brian James <brianjamesdlcz@gmail.com>
2024-08-31 21:20:09 +0800

    @86eq4e06a #Added unit testing on data grid

---
+++ packages/core-library/tests/components/DataGrid/DataGrid.test.tsx
@@ -0,0 +1,56 @@
+import { render, screen } from "../../common";
+import { DataGrid } from "../../../components";
+import { GridColDef } from "@mui/x-data-grid";
+
+jest.mock("../../../config", () => ({
+  getConfig: jest
+    .fn()
+    .mockReturnValue({ publicRuntimeConfig: { processEnv: {} } }),
+  config: { value: jest.fn() },
+}));
+
+jest.mock("../../../core/router", () => ({
+  useRouter: jest.fn(),
+}));
+
+describe('DataGrid Component', () => {
+  const mockColumns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 150 },
+    { field: 'name', headerName: 'Name', width: 150 },
+  ];
+
+  const mockRows = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Doe' },
+  ];
+
+  it('should render with given rows and columns', () => {
+    render(<DataGrid rows={mockRows} columns={mockColumns} isLoading={false} initPageSize={10} />);
+
+    expect(screen.getByRole('grid')).toBeInTheDocument();
+    expect(screen.getByText('John Doe')).toBeInTheDocument();
+    expect(screen.getByText('Jane Doe')).toBeInTheDocument();
+  });
+
+  it('should handle the columns and rows with an empty array ', () => {
+    render(<DataGrid rows={[]} columns={[]} isLoading={false} initPageSize={10} />);
+
+    expect(screen.getByRole('grid')).toBeInTheDocument();
+    expect(screen.queryByText("John Doe")).not.toBeInTheDocument();
+    expect(screen.queryByText("Jane Doe")).not.toBeInTheDocument();
+  });
+
+  it('should render with loading state', () => {
+    render(<DataGrid rows={[]} columns={mockColumns} isLoading={true} initPageSize={10} />);
+
+    expect(screen.getByRole('progressbar')).toBeInTheDocument();
+  });
+
+  it('should disable column menu and row selection', () => {
+    render(<DataGrid rows={mockRows} columns={mockColumns} isLoading={false} initPageSize={10} />);
+
+    const grid = screen.getByRole('grid');
+    expect(grid).toHaveAttribute('aria-multiselectable', 'false');
+  });
+
+});
