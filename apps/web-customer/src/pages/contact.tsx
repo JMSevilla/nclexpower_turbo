1726cd5dbdd9fe667c9bd861a4ab5cd72922f854
JezChristian <bacasnojezreel@gmail.com>
2024-08-31 22:18:30 +0800

    @86eq38yjm #CSP Implementation in WC Pages

--- apps/web-customer/src/pages/contact.tsx
+++ apps/web-customer/src/pages/contact.tsx
@@ -1,11 +1,23 @@
+import CSPHead from "core-library/components/CSPHead";
 import { ContactFormBlock } from "../components/blocks/ContactBlock/ContactFormBlock";
 import { ContactHero } from "../components/blocks/ContactBlock/ContactHero";
+import { GetServerSideProps } from "next";
+import { withCSP } from "core-library";
 
-export default function ContactPage() {
+interface Props {
+  generatedNonce: string;
+}
+
+ const ContactPage:React.FC<Props> = ({generatedNonce}) => {
   return (
     <div>
+      <CSPHead nonce={generatedNonce} />
       <ContactHero />
       <ContactFormBlock />
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = withCSP();
+
+export default ContactPage
