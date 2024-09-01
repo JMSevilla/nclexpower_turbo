559255a27d2da7b50a20ebf7ee8c2989a138f370
Brian James <brianjamesdlcz@gmail.com>
2024-09-01 12:42:30 +0800

    @86eq4e06a #Fixing build failure

--- packages/core-library/tests/components/DataGrid/DataGrid.test.tsx
+++ packages/core-library/tests/components/DataGrid/DataGrid.test.tsx
@@ -11,8 +11,8 @@ jest.mock("../../../core/router", () => ({
 
 describe('DataGrid Component', () => {
   const mockColumns = [
-    { field: 'id', headerName: 'ID', width: 150 },
-    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'id', headerName: 'ID' },
+    { field: 'name', headerName: 'Name' },
   ];
 
   const mockRows = [
@@ -22,35 +22,61 @@ describe('DataGrid Component', () => {
 
   it('should render with given rows and columns', () => {
     act(() => {
-      render(<DataGrid rows={mockRows} columns={mockColumns} isLoading={false} initPageSize={10} />);
-    })
-
-    expect(screen.getByRole('grid')).toBeInTheDocument();
-    expect(screen.getByText('John Doe')).toBeInTheDocument();
-    expect(screen.getByText('Jane Doe')).toBeInTheDocument();
+      render(
+        <DataGrid
+          rows={mockRows}
+          columns={mockColumns}
+          isLoading={false}
+          initPageSize={10}
+        />
+      );
+    });
+    expect(screen.getByRole('grid')).toBeTruthy();
+    expect(screen.getByText('John Doe')).toBeTruthy();
+    expect(screen.getByText('Jane Doe')).toBeTruthy();
   });
 
   it('should handle the columns and rows with an empty array ', () => {
     act(() => {
-      render(<DataGrid rows={[]} columns={[]} isLoading={false} initPageSize={10} />);
-
-      expect(screen.queryByText("John Doe")).not.toBeInTheDocument();
-      expect(screen.queryByText("Jane Doe")).not.toBeInTheDocument();
-    })
+      render(
+        <DataGrid
+          rows={[]}
+          columns={[]}
+          isLoading={false}
+          initPageSize={10}
+        />
+      );
+    });
+    expect(screen.queryByText("John Doe")).toBeFalsy();
+    expect(screen.queryByText("Jane Doe")).toBeFalsy();
   });
 
   it('should render with loading state', () => {
     act(() => {
-      render(<DataGrid rows={[]} columns={mockColumns} isLoading={true} initPageSize={10} />);
-    })
+      render(
+        <DataGrid
+          rows={[]}
+          columns={mockColumns}
+          isLoading={true}
+          initPageSize={10}
+        />
+      );
+    });
 
     expect(screen.getByRole('progressbar')).toBeInTheDocument();
   });
 
   it('should disable column menu and row selection', () => {
     act(() => {
-      render(<DataGrid rows={mockRows} columns={mockColumns} isLoading={false} initPageSize={10} />);
-    })
+      render(
+        <DataGrid
+          rows={mockRows}
+          columns={mockColumns}
+          isLoading={false}
+          initPageSize={10}
+        />
+      );
+    });
 
     const grid = screen.getByRole('grid');
     expect(grid).toHaveAttribute('aria-multiselectable', 'false');
