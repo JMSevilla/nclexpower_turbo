1e4bbd116e097ebafd3a27be33c7237cb43eda82
nasolgabriel <nasolgabrielr@gmail.com>
2024-09-02 10:05:07 +0800

    @86eq30169 #improved tabs

--- apps/web-backoffice-generic/src/pages/hub/acam/question-approval.tsx
+++ apps/web-backoffice-generic/src/pages/hub/acam/question-approval.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import withAuth from "core-library/core/utils/withAuth";
-import { Alert, Tabs } from "core-library/components";
+import { Alert, InformationTitle, Tabs } from "core-library/components";
+import { Box } from '@mui/material';
 
 const questionTabs = [
   { id: 1, title: 'Regular', content: 'Regular Questions' },
@@ -9,12 +10,32 @@ const questionTabs = [
 
 const TabContent: React.FC = () => {
   return (
-    <div>
-      <Tabs tabsItem={questionTabs} />
-      <Alert
-        severity="info"
-        title="contents" />
-    </div>
+    <Box sx={{ mb: 5 }}>
+      <InformationTitle
+          text="Content Approval"
+          lineWidth={6}
+          lineHeight={35}
+          lineColor="#6A5ACD"
+          borderRadius={2}
+          containerProps={{ mb: 5 }}
+          textProps={{ color: "text.primary", fontWeight: "bold" }}
+      />
+      <Box 
+        sx={{
+          border: '1px solid #ddd',
+          borderRadius: 2,
+          p: 2,
+          boxShadow: 3,
+          backgroundColor: '#fff',
+          display: 'flex',
+          justifyContent: 'flex-start', // Align content to the left
+        }}
+      >
+        <Box sx={{ flexGrow: 1 }}> {/* Ensure the Tabs are aligned to the left */}
+          <Tabs tabsItem={questionTabs} />
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
