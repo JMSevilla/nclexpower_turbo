cf6f3fa989693e06b69ca8c4b6d0132e17530f97
JezChristian <bacasnojezreel@gmail.com>
2024-09-03 01:58:19 +0800

    @86eq3ij3j #Regex Implementation to Selected TextFields

--- packages/core-library/components/Dialog/DialogFormBlocks/category/validation.ts
+++ packages/core-library/components/Dialog/DialogFormBlocks/category/validation.ts
@@ -1,8 +1,9 @@
 import * as yup from "yup";
+import { TEXT_REGEX } from "../../../../utils";
 
 export const categorySchema = yup.object({
-  categoryName: yup.string().required("category name is required").default(""),
-  categoryDescription: yup.string().notRequired().default(""),
+  categoryName: yup.string().required("category name is required").default("").matches(TEXT_REGEX, "Special characters not allowed"),
+  categoryDescription: yup.string().notRequired().default("").matches(TEXT_REGEX, "Special characters not allowed"),
   categoryType: yup.number().required("Kindly select category type").default(0),
 });
 
