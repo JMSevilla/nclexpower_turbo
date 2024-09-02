4e97042bab90981fcd2aeea31b553ce195197107
Ederson Marcial <101306289+EderDM@users.noreply.github.com>
2024-09-03 00:25:26 +1000

    Revert "@86eq67r3v #404-page-not-found-unresponsive"

--- apps/web-customer/src/components/blocks/NotFoundBlock/NotFoundBlock.tsx
+++ apps/web-customer/src/components/blocks/NotFoundBlock/NotFoundBlock.tsx
@@ -11,27 +11,27 @@ export const NotFoundBlock: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col md:flex-row items-center justify-center font-['Poppins'] text-center p-4">
-      <div className="w-full md:w=[10%] max-w-[643.5px] h-auto md:h-[637px] flex-shrink-5 mb-8 md:mb-0 md:mr-10 mt-20 md:mt-0">
-        <Image src={NotFoundPage} alt="Page Not Found" className="w-full h-auto object-contain"/>
+    <div className="h-screen flex items-center justify-center font-['Poppins'] text-center p-4 ">
+      <div className="w-[643.5px] h-[637px] flex-shrink-0 mr-10">
+        <Image src={NotFoundPage} alt="Page Not Found" />
       </div>
       <div className="flex flex-col gap-y-2 justify-center items-center">
-        <h1 className="text-[#3562D8] font-Poppins text-center text-[70px] md:text-[120px] font-bold leading-none md:leading-[108%]">
+        <h1 className="W-[547px] text-[#3562D8] font-Poppins text-center text-[200px] font-bold leading-[108%]">
           404
         </h1>
 
-        <h1 className="font-Rajdhani text-center text-[#232323] text-[36px] md:text-[80px] font-bold leading-none md:leading-[100%] pb-4">
+        <h1 className="font-Rajdhani text-center text-[#232323] text-[80px] font-bold leading-[100%] pb-4">
           Page not found
         </h1>
 
-        <p className="text-center text-[16px] md:text-[23px] font-ptSans font-normal leading-[24px] md:leading-[36px] text-[#9A9A9A] max-w-[560px] pb-3">
+        <p className="text-center text-[23px] font-ptSans font-normal leading-[36px] text-[#9A9A9A] w-[560px] pb-3">
           Were’ sorry, the page you requested could not be found. Please go back
           to the previous page.
         </p>
 
         <div className="flex justify-center">
           <button
-            className="mt-4 flex w-[100px] md:w-[120px] h-[45px] md:h-[55px] py-[10px] md:py-[13px] justify-center items-center flex-shrink-0 bg-[#0F2A71] rounded-[10px] text-[#FFFDF1] text-center font-ptSansNarrow font-bold leading-none md:leading-[36px]"
+            className="mt-4 flex w-[120px] h-[55px] py-[13px] justify-center items-center flex-shrink-0 bg-[#0F2A71] rounded-[10px] text-[#FFFDF1] text-center font-ptSansNarrow font-bold leading-[36px]"
             onClick={Back}
           >
             GO BACK
