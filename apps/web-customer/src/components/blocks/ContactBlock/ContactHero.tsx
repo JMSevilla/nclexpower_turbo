689c08649f5fcfee983e6162d7ef8be8cccb0dc2
bryan <bry@payloro.com>
2024-09-07 01:15:39 +0800

    @86eq79x58 #Fix WC Contact Us Page UI

--- apps/web-customer/src/components/blocks/ContactBlock/ContactHero.tsx
+++ apps/web-customer/src/components/blocks/ContactBlock/ContactHero.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 export const ContactHero: React.FC = () => {
   return (
         <Box className="h-auto" position="relative">
-          <div className="relative h-[633px]">
-            <Image className='w-full h-[633px] relative object-cover' loading='lazy' src={ContactUsBanner} alt="ContactUsBanner" />
+          <div className="relative h-screen lg:h-[633px]">
+            <Image className='w-full h-screen lg:h-[633px] relative object-cover' loading='lazy' src={ContactUsBanner} alt="ContactUsBanner" data-testid="contactus-banner"/>
             <div className="absolute inset-0 z-10 flex flex-col items-center justify-center space-y-4 lg:items-start lg:justify-start lg:ml-[12%] lg:mt-[12%]">
               <p className="lg:text-6xl text-4xl font-ptSans text-yellow font-bold text-center lg:text-left">
                 Get In Touch
