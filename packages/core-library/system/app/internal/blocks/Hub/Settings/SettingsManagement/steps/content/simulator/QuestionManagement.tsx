829d5b367ca3f8c225502db6e855feead5ef672a
costales arnel <costalesarnel08@gmail.com>
2024-09-08 14:30:36 +0800

    @86eq7pmwe #Regular question steps not reset if back button clicked

--- packages/core-library/system/app/internal/blocks/Hub/Settings/SettingsManagement/steps/content/simulator/QuestionManagement.tsx
+++ packages/core-library/system/app/internal/blocks/Hub/Settings/SettingsManagement/steps/content/simulator/QuestionManagement.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo } from "react";
 import {
   Button,
   Card,
@@ -9,23 +9,44 @@ import {
 import { SettingsSelectionType } from "../../../types";
 import { useModal } from "core-library/hooks";
 import { useQuestionManagementWizardSteps } from "./steps/useSteps";
+import { useRouter } from "../../../../../../../../../../core";
 
 interface Props {
   nextStep(values: Partial<SettingsSelectionType>): void;
   previousStep(): void;
   values: Partial<SettingsSelectionType>;
   previous: () => void;
+  reset: () => void;
 }
 
 export const QuestionManagement: React.FC<Props> = ({
   previousStep,
   previous,
+  reset,
 }) => {
+  const router = useRouter();
   const saveConfirmationModal = useModal<unknown>();
   const { render } = useQuestionManagementWizardSteps(
     () => {},
     saveConfirmationModal
   );
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if (
+        router.pathname === "/hub/settings/internal-application-settings" &&
+        url !== "/hub/settings/internal-application-settings"
+      ) {
+        reset();
+      }
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, [router]);
 
   const tabs = useMemo<Array<TabOption>>(
     () => [
@@ -39,6 +60,7 @@ export const QuestionManagement: React.FC<Props> = ({
   function handlePrevious() {
     previous();
     previousStep();
+    reset();
   }
   return (
     <Card>
