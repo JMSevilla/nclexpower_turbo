f666c43f8aee89e5aa4fbd0f388b66df4d142630
Samuel <samueljavier1991@gmail.com>
2024-09-12 00:38:01 +0800

    @86eq5wrg2#header-dropdown-ui-bug ft. After logout application router stays in /hub

--- packages/core-library/components/GenericDropDown/AccountMenu.tsx
+++ packages/core-library/components/GenericDropDown/AccountMenu.tsx
@@ -5,13 +5,10 @@ import { Logout as LogoutIcon } from "@mui/icons-material";
 import { NavigationType } from "../../types/navigation";
 import { useState } from "react";
 import { Button } from "../Button/Button";
-
 interface Props {
   label: string;
   icon?: React.ReactNode;
   accountItem: NavigationType[];
-  anchorEl?: HTMLElement | null;
-  onClick?: (event: React.MouseEvent<HTMLElement>) => void;
   onLogout?: () => void;
 }
 
@@ -19,24 +16,28 @@ export const AccountMenu: React.FC<Props> = ({
   icon,
   label,
   accountItem,
-  anchorEl,
-  onClick,
   onLogout,
 }) => {
+
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const [expandedIndex, setExpandedIndex] = useState<null | number>(null);
+
   const openMenu = Boolean(anchorEl);
   const id = openMenu ? "simple-popper" : undefined;
-  const [expandedIndex, setExpandedIndex] = useState<null | number>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
 
   const handleParentClick = (index: number) => {
     setExpandedIndex(expandedIndex === index ? null : index);
   };
-
   return (
     <Box>
       <Button
         sx={{ gap: 2 }}
         aria-describedby={id}
-        onClick={onClick}
+        onClick={handleClick}
         variant="outlined"
         data-testid="account-menu-button"
       >
@@ -83,7 +84,9 @@ export const AccountMenu: React.FC<Props> = ({
                       }}
                     >
                       {subMenu.icon}{" "}
-                      <Typography variant="button"> {subMenu.label}</Typography>
+                      <Typography variant="button">
+                        {subMenu.label}
+                      </Typography>
                     </Button>
                   ))}
               </div>
