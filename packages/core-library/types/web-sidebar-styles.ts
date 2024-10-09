f7ec519b693b8f8ab27af7f43876df2841b35963
Samuel <samueljavier1991@gmail.com>
2024-10-10 01:18:28 +0800

    @86eqeb76p#sidebar-refactor-address-comments

--- packages/core-library/types/web-sidebar-styles.ts
+++ packages/core-library/types/web-sidebar-styles.ts
@@ -1,14 +1,15 @@
 import { SxProps } from '@mui/material';
 import { Theme } from '@mui/material/styles';
 
-
 export interface WebSidebarStylesType {
-    sidebarSx: SxProps<Theme>;
-    arrowSx: SxProps<Theme>;
-    dividerSx: SxProps<Theme>;
-    listItemIconSx: SxProps<Theme>;
-    paddingSx: SxProps<Theme>;
-    activeSx: SxProps<Theme>;
-    hoverSx: SxProps<Theme>;
-    opacitySx: SxProps<Theme>;
+    listStyles: {
+        sidebarSx: SxProps<Theme>;
+        arrowSx: SxProps<Theme>;
+        dividerSx: SxProps<Theme>;
+        listItemIconSx: SxProps<Theme>;
+        paddingSx: SxProps<Theme>;
+        activeSx: SxProps<Theme>;
+        hovericonSx: SxProps<Theme>;
+        opacitySx: SxProps<Theme>;
+    };
 }
