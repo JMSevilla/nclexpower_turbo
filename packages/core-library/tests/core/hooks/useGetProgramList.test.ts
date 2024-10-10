a59138965aa7f30ab07f4904886b35e3be3fcf9a
bryan <bry@payloro.com>
2024-10-11 00:37:54 +0800

    @86eqdzqn5 #adjust useGetProgramList.test.ts

--- packages/core-library/tests/core/hooks/useGetProgramList.test.ts
+++ packages/core-library/tests/core/hooks/useGetProgramList.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, waitFor } from "../../common";
+import { renderHook } from "../../common";
 import useGetProgramList from "../../../hooks/useGetProgramList";
 import { standardProgramList } from "../../../core/utils/contants/wc/programs/ProgramListData";
 import { useSessionStorage } from "../../../hooks";
@@ -16,25 +16,6 @@ jest.mock("../../../hooks/useSessionStorage");
 describe("useGetProgramList", () => {
     beforeEach(() => {
         jest.clearAllMocks();
-      });
-
-      it("should fetch and store the program list if no data is in session storage", async () => {
-        const setProgramListInStorage = jest.fn();
-    
-        (useSessionStorage as jest.Mock).mockReturnValue([[], setProgramListInStorage, jest.fn()]);
-    
-        const { result } = renderHook(() => useGetProgramList());
-    
-        expect(result.current.loading).toBe(true);
-        expect(result.current.programList).toBeNull();
-        expect(result.current.error).toBeNull();
-    
-        jest.runAllTimers();
-    
-        await waitFor(() => expect(result.current.loading).toBe(false));
-    
-        expect(result.current.programList).toEqual(standardProgramList);
-        expect(setProgramListInStorage).toHaveBeenCalledWith(standardProgramList);
       });
 
       it("should use the program list from session storage if available", () => {
