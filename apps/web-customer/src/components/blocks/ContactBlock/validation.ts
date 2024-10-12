01a51d95c7c48881cb3fdcff0917f9d3b132479f
emmanlemon <emmanuellemon111200@gmail.com>
2024-10-12 11:22:22 +0800

    @86eqemm1q#WC- Contact us Change Text Field to Phone Field

--- apps/web-customer/src/components/blocks/ContactBlock/validation.ts
+++ apps/web-customer/src/components/blocks/ContactBlock/validation.ts
@@ -2,18 +2,27 @@ import { STRING_REGEX } from "core-library";
 import * as yup from "yup";
 
 export const contactSchema = yup.object({
-  name: yup.string().required("Name is required").default("").matches(STRING_REGEX, "Special characters not allowed"),
-  phone: yup  
+  name: yup
+    .string()
+    .required("Name is required")
+    .default("")
+    .matches(STRING_REGEX, "Special characters not allowed"),
+  phone: yup
     .string()
     .matches(/^\d*$/, "Phone number must be a valid number")
     .required("Phone number is required")
     .default(""),
+  countryCode: yup.string().required("Country Code is required").default("PH"),
   email: yup
     .string()
     .email("Invalid email")
     .required("Email is required")
     .default(""),
-  message: yup.string().required("Message is required").default("").matches(STRING_REGEX, "Special characters not allowed"),
+  message: yup
+    .string()
+    .required("Message is required")
+    .default("")
+    .matches(STRING_REGEX, "Special characters not allowed"),
 });
 
 export type ContactFormType = yup.InferType<typeof contactSchema>;
