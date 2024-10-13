ee53c60dd84130c7f98138ba6a91b342bef464e1
mrkmartin <markpaulomartin@gmail.com>
2024-10-13 23:25:36 +0800

    @86eqftzx2 #adjust the link color to white

--- packages/core-library/components/ReusableFooter/Footer.tsx
+++ packages/core-library/components/ReusableFooter/Footer.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
-import { NCLEXYellowLogo } from "../../assets";
-import { FooterProps } from "../../types/global";
-import { useMemo } from "react";
-import { Box, Button, Grid, Typography } from "@mui/material";
-import { useRouteBasedVisibility } from "../../hooks";
-import { HideFooter } from "./HideFooter";
-import { useRouter } from "../../core";
+import Image from 'next/image';
+import { NCLEXYellowLogo } from '../../assets';
+import { FooterProps } from '../../types/global';
+import { useMemo } from 'react';
+import { Box, Button, Grid, Typography } from '@mui/material';
+import { useRouteBasedVisibility } from '../../hooks';
+import { HideFooter } from './HideFooter';
+import { useRouter } from '../../core';
 
 export const Footer: React.FC<FooterProps> = (props) => {
   const yearData = new Date().getFullYear();
@@ -13,7 +13,7 @@ export const Footer: React.FC<FooterProps> = (props) => {
   const router = useRouter();
 
   const updatedHideFooter =
-    HideFooter.includes(router.pathname) || router.pathname?.startsWith("/hub")
+    HideFooter.includes(router.pathname) || router.pathname?.startsWith('/hub')
       ? [...HideFooter, router.pathname]
       : HideFooter;
 
@@ -24,11 +24,11 @@ export const Footer: React.FC<FooterProps> = (props) => {
       <Box
         width={1}
         sx={{
-          padding: " 2.5rem",
-          color: "white",
-          backgroundColor: "#040814",
+          padding: ' 2.5rem',
+          color: 'white',
+          backgroundColor: '#040814',
           paddingY: 5,
-          fontFamily: "PT Sans",
+          fontFamily: 'PT Sans',
           flexGrow: 1,
         }}
       >
@@ -37,8 +37,8 @@ export const Footer: React.FC<FooterProps> = (props) => {
           spacing={{ xs: 2, md: 1 }}
           columns={{ xs: 2, sm: 2, md: 13 }}
           sx={{
-            display: "flex",
-            width: "100%",
+            display: 'flex',
+            width: '100%',
           }}
         >
           {props.list.length > 0 && (
@@ -48,17 +48,17 @@ export const Footer: React.FC<FooterProps> = (props) => {
               sm={12}
               md={4}
               sx={{
-                display: "flex",
-                alignItems: "start",
-                justifyContent: "center",
-                width: "25%",
+                display: 'flex',
+                alignItems: 'start',
+                justifyContent: 'center',
+                width: '25%',
                 gap: 5,
               }}
             >
               <Image
                 style={{ width: 150 }}
                 src={NCLEXYellowLogo}
-                alt="NCLEXLogo"
+                alt='NCLEXLogo'
               />
             </Grid>
           )}
@@ -68,10 +68,10 @@ export const Footer: React.FC<FooterProps> = (props) => {
             sm={12}
             md={8}
             sx={{
-              display: "flex",
-              flexDirection: { xs: "column", sm: "row" },
-              alignItems: "start",
-              width: "100%",
+              display: 'flex',
+              flexDirection: { xs: 'column', sm: 'row' },
+              alignItems: 'start',
+              width: '100%',
             }}
           >
             <Grid
@@ -80,11 +80,11 @@ export const Footer: React.FC<FooterProps> = (props) => {
               sm={7}
               md={5}
               sx={{
-                width: "100%",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: { xs: "center", sm: "start" },
-                textAlign: { xs: "center", sm: "start" },
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: { xs: 'center', sm: 'start' },
+                textAlign: { xs: 'center', sm: 'start' },
               }}
             >
               <Typography sx={{ marginBottom: 2 }}>
@@ -94,28 +94,28 @@ export const Footer: React.FC<FooterProps> = (props) => {
                 sx={{
                   marginBottom: { xs: 2, md: 6 },
                   gap: 2,
-                  width: "70%",
-                  display: "flex",
-                  justifyContent: "space-between",
-                  flexDirection: { xs: "row", sm: "column" },
+                  width: '70%',
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  flexDirection: { xs: 'row', sm: 'column' },
                 }}
               >
                 <Typography
                   sx={{
-                    fontSize: "12px",
-                    width: "fit-content",
+                    fontSize: '12px',
+                    width: 'fit-content',
                     borderBottom: 2,
-                    borderBlockColor: "#f5c206",
+                    borderBlockColor: '#f5c206',
                   }}
                 >
                   {props.info.phone}
                 </Typography>
                 <Typography
                   sx={{
-                    fontSize: "12px",
-                    width: "fit-content",
+                    fontSize: '12px',
+                    width: 'fit-content',
                     borderBottom: 2,
-                    borderBlockColor: "#f5c206",
+                    borderBlockColor: '#f5c206',
                   }}
                 >
                   {props.info.website}
@@ -128,10 +128,10 @@ export const Footer: React.FC<FooterProps> = (props) => {
               sm={5}
               md={4}
               sx={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "space-evenly",
-                alignItems: "start",
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'space-evenly',
+                alignItems: 'start',
                 marginBottom: 2,
               }}
             >
@@ -142,7 +142,9 @@ export const Footer: React.FC<FooterProps> = (props) => {
                       {props.list.length > 0 &&
                         list.items.map((item, index) => (
                           <li key={index}>
-                            <a href={item.path}>{item.label}</a>
+                            <a href={item.path} className='text-white'>
+                              {item.label}
+                            </a>
                           </li>
                         ))}
                     </ul>
@@ -151,12 +153,12 @@ export const Footer: React.FC<FooterProps> = (props) => {
             </Grid>
           </Grid>
         </Grid>
-        <div className="text-xs">
-          <p className="w-full text-center pt-4">
+        <div className='text-xs'>
+          <p className='w-full text-center pt-4'>
             NCLEX-RN® and NCLEX-PN® are registered trademarks of the National
             Council of State Boards of Nursing, Inc (NCSBN®)
           </p>
-          <p className="w-full text-center pt-4">
+          <p className='w-full text-center pt-4'>
             © {memoYear} NCLEXPower ™. All rights reserved.
           </p>
         </div>
