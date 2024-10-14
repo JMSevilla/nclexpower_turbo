6964083627fae63b9f88d3c522f0c9d2c77afb60
JezChristian <bacasnojezreel@gmail.com>
2024-10-14 14:07:50 +0800

    86eq8pbvd #In App Form Restructure - Code Cov Test

--- packages/core-library/tests/components/icon-component.test.tsx
+++ packages/core-library/tests/components/icon-component.test.tsx
@@ -52,4 +52,46 @@ describe("IconComponent", () => {
     const icon = screen.getByTestId("SourceIcon");
     expect(icon).toBeInTheDocument();
   });
+
+  it("should render QuestionManagementIcon when 'QuestionManagementIcon' is passed", () => {
+    renderIcon("QuestionManagementIcon");
+    const icon = screen.getByTestId("QuestionManagementIcon");
+    expect(icon).toBeInTheDocument();
+  });
+
+  it("should render ResultsIcon when 'ResultsIcon' is passed", () => {
+    renderIcon("ResultsIcon");
+    const icon = screen.getByTestId("ResultsIcon");
+    expect(icon).toBeInTheDocument();
+  });
+
+  it("should render DeleteIcon when 'DeleteIcon' is passed", () => {
+    renderIcon("DeleteIcon");
+    const icon = screen.getByTestId("DeleteIcon");
+    expect(icon).toBeInTheDocument();
+  });
+
+  it("should render ApprovalIcon when 'ApprovalIcon' is passed", () => {
+    renderIcon("ApprovalIcon");
+    const icon = screen.getByTestId("ApprovalIcon");
+    expect(icon).toBeInTheDocument();
+  });
+
+  it("should render keySquare when 'keySquare' is passed", () => {
+    renderIcon("keySquare");
+    const icon = screen.getByTestId("keySquare");
+    expect(icon).toBeInTheDocument();
+  });
+
+  it("should render TRDSquare when 'TRDSquare' is passed", () => {
+    renderIcon("TRDSquare");
+    const icon = screen.getByTestId("TRDSquare");
+    expect(icon).toBeInTheDocument();
+  });
+
+  it("should render DotsIcon when 'DotsIcon' is passed", () => {
+    renderIcon("DotsIcon");
+    const icon = screen.getByTestId("DotsIcon");
+    expect(icon).toBeInTheDocument();
+  });
 });
