b50702271f41f1fcc01bafbf08c1036c0302f1ac
Emmanuel Joshua A. Lemon <emmanuel.lemon@safc.com.ph>
2024-10-16 08:15:09 +0800

    @86eqemm1q#Added Comment on every file changed

--- apps/web-customer/src/components/blocks/ContactBlock/validation.ts
+++ apps/web-customer/src/components/blocks/ContactBlock/validation.ts
@@ -1,3 +1,8 @@
+/**
+* Property of the NCLEX Power.
+* Reuse as a whole or in part is prohibited without permission.
+* Created by the Software Strategy & Development Division
+*/
 import { STRING_REGEX } from "core-library";
 import { DEFAULT_PHONE_COUNTRY_CODE } from "core-library/types/constant";
 import * as yup from "yup";
