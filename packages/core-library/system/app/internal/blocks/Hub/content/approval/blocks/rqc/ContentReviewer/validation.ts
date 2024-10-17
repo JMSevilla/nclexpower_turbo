90e2fe9d1a34c58f09e77928a63c49e351743f93
Brian James (BJ) <brianjamesdlcz@gmail.com>
2024-10-17 10:52:15 +0800

    @86eqgzrwu #Fix disabled props to be string or undefined

--- packages/core-library/system/app/internal/blocks/Hub/content/approval/blocks/rqc/ContentReviewer/validation.ts
+++ packages/core-library/system/app/internal/blocks/Hub/content/approval/blocks/rqc/ContentReviewer/validation.ts
@@ -1,3 +1,8 @@
+/**
+* Property of the NCLEX Power.
+* Reuse as a whole or in part is prohibited without permission.
+* Created by the Software Strategy & Development Division
+*/
 import * as yup from 'yup';
 
 export const crbSchema = yup.object({
