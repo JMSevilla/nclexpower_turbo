39aeda4b07b756239475e13db8df9ca1aa42cada
Emmanuel Joshua A. Lemon <emmanuel.lemon@safc.com.ph>
2024-10-19 23:25:02 +0800

    @86eqcxfwc - Fix Run Build

--- packages/core-library/core/utils/IconUtils/IconUtils.ts
+++ packages/core-library/core/utils/IconUtils/IconUtils.ts
@@ -1,49 +1,54 @@
 import {
-    CompletedWhiteIcon,
-    InProgressIcon,
-    InProgressWhiteIcon,
-    AvailableIcon,
-    CompletedIcon,
-    DocumentIcon,
-    VideosIcon,
-    SimulatorIcon,
-    CardsIcon,
-    PerspectiveIcon,
-    StrategyIcon,
-    CATIcon,
-  } from "core-library/assets";
+  CompletedWhiteIcon,
+  InProgressIcon,
+  InProgressWhiteIcon,
+  AvailableIcon,
+  CompletedIcon,
+  DocumentIcon,
+  VideosIcon,
+  SimulatorIcon,
+  CardsIcon,
+  PerspectiveIcon,
+  StrategyIcon,
+  CATIcon,
+} from "../../../assets";
 
 const statusIcons = {
-    completed: CompletedWhiteIcon,
-    progress: InProgressWhiteIcon,
-    available: AvailableIcon,
-  } as const; 
-  
-  const sectionTypeIcons = {
-    document: DocumentIcon,
-    video: VideosIcon,
-    simulator: SimulatorIcon,
-    "content-cards": CardsIcon,
-    "med-cards": CardsIcon,
-    perspective: PerspectiveIcon,
-    strategy: StrategyIcon,
-    CAT: CATIcon
-  } as const;
+  completed: CompletedWhiteIcon,
+  progress: InProgressWhiteIcon,
+  available: AvailableIcon,
+} as const;
 
-  const sectionStatusIcons = {
-    completed: CompletedIcon,
-    "in-progress": InProgressIcon,
-    available: AvailableIcon
-  } as const;
+const sectionTypeIcons = {
+  document: DocumentIcon,
+  video: VideosIcon,
+  simulator: SimulatorIcon,
+  "content-cards": CardsIcon,
+  "med-cards": CardsIcon,
+  perspective: PerspectiveIcon,
+  strategy: StrategyIcon,
+  CAT: CATIcon,
+} as const;
 
-  export const getStatusIcons = (status: string) => {
-    return statusIcons[status as keyof typeof statusIcons] || AvailableIcon;
-  };
+const sectionStatusIcons = {
+  completed: CompletedIcon,
+  "in-progress": InProgressIcon,
+  available: AvailableIcon,
+} as const;
 
-  export const getSectionTypeIcons = (type: string) => {
-    return sectionTypeIcons[type as keyof typeof sectionTypeIcons] || DocumentIcon
-  }
+export const getStatusIcons = (status: string) => {
+  return statusIcons[status as keyof typeof statusIcons] || AvailableIcon;
+};
 
-  export const getSectionStatusIcons = (status: string) => {
-    return sectionStatusIcons[status as keyof typeof sectionStatusIcons] || AvailableIcon;
-  }
+export const getSectionTypeIcons = (type: string) => {
+  return (
+    sectionTypeIcons[type as keyof typeof sectionTypeIcons] || DocumentIcon
+  );
+};
+
+export const getSectionStatusIcons = (status: string) => {
+  return (
+    sectionStatusIcons[status as keyof typeof sectionStatusIcons] ||
+    AvailableIcon
+  );
+};
