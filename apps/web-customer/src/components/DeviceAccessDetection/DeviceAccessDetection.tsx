adf591c77368c316403a3868a4658380ad29ee87
Brian James (BJ) <brianjamesdlcz@gmail.com>
2024-10-19 22:07:34 +0800

    @86eqeamnu #Remove Image on Mobile Device Request

--- apps/web-customer/src/components/DeviceAccessDetection/DeviceAccessDetection.tsx
+++ apps/web-customer/src/components/DeviceAccessDetection/DeviceAccessDetection.tsx
@@ -10,133 +10,107 @@ interface Props {
 }
 
 export const DeviceDetectionDialog = () =>
-  // details:Props //Uncomment this in actual implementation
-  {
-    const details: Props = {
-      model: "IPhone 15 Pro Max",
-      location: "Angeles, Pampanga",
-    };
+// details:Props //Uncomment this in actual implementation
+{
+  const details: Props = {
+    model: "IPhone 15 Pro Max",
+    location: "Angeles, Pampanga",
+  };
 
-    const [isGranted, setIsGrant] = useState<boolean>(false);
-    const handleApprove = () => {
-      console.log("Approve", "isGranted : ", isGranted);
-    };
-    const handleDecline = () => {
-      console.log("Declined", "isGranted : ", isGranted);
-    };
+  const [isGranted, setIsGrant] = useState<boolean>(false);
+  const handleApprove = () => {
+    console.log("Approve", "isGranted : ", isGranted);
+  };
+  const handleDecline = () => {
+    console.log("Declined", "isGranted : ", isGranted);
+  };
 
-    return (
-      <Box sx={{ width: "100dvw", height: "100dvh" }}>
-        <Grid container sx={{ width: "100%", display: "flex", height: "100%" }}>
-          <Grid
-            lg={6}
-            sm={12}
-            item
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              paddingX: "80px",
-              paddingTop: "80px",
-            }}
+  return (
+    <div className="w-full h-screen flex items-center justify-center md:flex-col lg:flex-row">
+      <div className="w-full hidden md:flex  items-center justify-center px-20 pt-20">
+        <Image alt="Detect Device" src={DetectDevice} width={400} className="hidden md:flex" />
+      </div>
+      <div className="w-full flex flex-col gap-2 h-full items-start justify-center px-6 ">
+        <Image alt="NCLEX Logo" src={NCLEXBlueLogo} />
+        <Box>
+          <Typography variant="h5" fontWeight={600} color="#0d2b71">
+            Device Transfer Notice
+          </Typography>
+          <Typography fontWeight={600} color="GrayText">
+            Someone is trying to access your account
+          </Typography>
+          <Typography fontSize="14px" fontWeight={600} color="GrayText">
+            Device Information :
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="h5" fontWeight={600}>
+            {details.model}
+          </Typography>
+          <Typography fontWeight={500}>
+            Location : {details.location}
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="h6" color="GrayText" fontWeight={600}>
+            This device is trying to access your account.
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            color="GrayText"
+            width="70%"
+            fontSize="14px"
+            lineHeight="20px"
           >
-            <Image alt="Detect Device" src={DetectDevice} width={350} />
-          </Grid>
-          <Grid
-            lg={6}
-            sm={12}
-            item
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              gap: 1,
-              height: "100%",
-              alignItems: "start",
-              justifyContent: "center",
-              paddingX: "25px",
-            }}
-          >
-            <Image alt="NCLEX Logo" src={NCLEXBlueLogo} />
-            <Box>
-              <Typography variant="h5" fontWeight={600} color="#0d2b71">
-                Device Transfer Notice
-              </Typography>
-              <Typography fontWeight={600} color="GrayText">
-                Someone is trying to access your account
-              </Typography>
-              <Typography fontSize="14px" fontWeight={600} color="GrayText">
-                Device Information :
-              </Typography>
-            </Box>
-            <Box>
-              <Typography variant="h5" fontWeight={600}>
+            Note: Approving a new device request will log out your current
+            primary device. Please confirm if you wish to proceed.
+          </Typography>
+          <Box display="flex" alignItems="center" justifyContent="start">
+            <Checkbox onChange={(e) => setIsGrant(e.target.checked)} />
+            <Typography marginLeft="-20px">
+              Grant this&nbsp;
+              <Typography component="span" fontWeight={500}>
                 {details.model}
               </Typography>
-              <Typography fontWeight={500}>
-                Location : {details.location}
-              </Typography>
-            </Box>
-            <Box>
-              <Typography variant="h6" color="GrayText" fontWeight={600}>
-                This device is trying to access your account.
-              </Typography>
-              <Typography
-                variant="subtitle1"
-                color="GrayText"
-                width="70%"
-                fontSize="14px"
-                lineHeight="20px"
-              >
-                Note: Approving a new device request will log out your current
-                primary device. Please confirm if you wish to proceed.
-              </Typography>
-              <Box display="flex" alignItems="center" justifyContent="start">
-                <Checkbox onChange={(e) => setIsGrant(e.target.checked)} />
-                <Typography marginLeft="-20px">
-                  Grant this&nbsp;
-                  <Typography component="span" fontWeight={500}>
-                    {details.model}
-                  </Typography>
-                  &nbsp;as primary device
-                </Typography>
-              </Box>
-            </Box>
-            <Grid container gap={2} sx={{ paddingY: "15px" }}>
-              <Grid item lg={3} md={5} sm={3} xs={12}>
-                <Button
-                  onClick={() => handleApprove()}
-                  fullWidth
-                  sx={{
-                    display: "flex",
-                    flexGrow: 1,
-                    padding: "10px",
-                    borderRadius: "10px",
-                    bgcolor: "green",
-                    "&:hover": { bgcolor: "#006000" },
-                  }}
-                >
-                  Approve
-                </Button>
-              </Grid>
-              <Grid item lg={3} md={5} sm={3} xs={12}>
-                <Button
-                  fullWidth
-                  onClick={() => handleDecline()}
-                  sx={{
-                    display: "flex",
-                    flexGrow: 1,
-                    padding: "10px",
-                    borderRadius: "10px",
-                    bgcolor: "red",
-                    "&:hover": { bgcolor: "#bf0000" },
-                  }}
-                >
-                  Decline
-                </Button>
-              </Grid>
-            </Grid>
+              &nbsp;as primary device
+            </Typography>
+          </Box>
+        </Box>
+        <Grid container gap={2} sx={{ paddingY: "15px" }}>
+          <Grid item lg={3} md={5} sm={3} xs={12}>
+            <Button
+              onClick={() => handleApprove()}
+              fullWidth
+              sx={{
+                display: "flex",
+                flexGrow: 1,
+                padding: "10px",
+                borderRadius: "10px",
+                bgcolor: "green",
+                "&:hover": { bgcolor: "#006000" },
+              }}
+            >
+              Approve
+            </Button>
+          </Grid>
+          <Grid item lg={3} md={5} sm={3} xs={12}>
+            <Button
+              fullWidth
+              onClick={() => handleDecline()}
+              sx={{
+                display: "flex",
+                flexGrow: 1,
+                padding: "10px",
+                borderRadius: "10px",
+                bgcolor: "red",
+                "&:hover": { bgcolor: "#bf0000" },
+              }}
+            >
+              Decline
+            </Button>
           </Grid>
         </Grid>
-      </Box>
-    );
-  };
+      </div>
+    </div>
+  );
+};
