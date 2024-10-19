59a303a91c468be7d68978c60a2ce3b5514c794b
Jezreel Christian Bacasno <bacasnojezreel@gmail.com>
2024-10-19 20:32:40 +0800

    @86eqeamyx #Added Property Disclaimer - Enhanced Error Display in Case Study Creation

--- packages/core-library/components/ErrorMap/ErrorMap.tsx
+++ packages/core-library/components/ErrorMap/ErrorMap.tsx
@@ -1,3 +1,8 @@
+/**
+ * Property of the NCLEX Power.
+ * Reuse as a whole or in part is prohibited without permission.
+ * Created by the Software Strategy & Development Division
+ */
 import { Box, Fade, Grow, Typography } from '@mui/material'
 import React, { useState } from 'react'
 import { FieldErrors } from 'react-hook-form'
--- packages/core-library/system/app/internal/blocks/Hub/Settings/SettingsManagement/steps/content/simulator/steps/content/casestudy/CaseStudyCreation/CreateCaseStudyQuestion.tsx
+++ packages/core-library/system/app/internal/blocks/Hub/Settings/SettingsManagement/steps/content/simulator/steps/content/casestudy/CaseStudyCreation/CreateCaseStudyQuestion.tsx
@@ -1,3 +1,8 @@
+/**
+ * Property of the NCLEX Power.
+ * Reuse as a whole or in part is prohibited without permission.
+ * Created by the Software Strategy & Development Division
+ */
 import React, { useEffect, useState } from "react";
 import { ContainedCaseStudyQuestionType } from "../../../../types";
 import {
