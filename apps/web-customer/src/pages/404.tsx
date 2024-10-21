aee2ac4feafad79092d98e1700c23e311c3d3a55
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2024-10-22 00:50:01 +0800

    @86eqh0bcq #Remove get server sie props 404

--- apps/web-customer/src/pages/404.tsx
+++ apps/web-customer/src/pages/404.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { NotFoundBlock } from "../components/blocks/NotFoundBlock/NotFoundBlock";
-import { GetServerSideProps } from "next";
-import { withCSP } from "core-library";
 
 export default function Custom404() {
   return <NotFoundBlock />;
 }
-
-export const getServerSideProps: GetServerSideProps = withCSP();
