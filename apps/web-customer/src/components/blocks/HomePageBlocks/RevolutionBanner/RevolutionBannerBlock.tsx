ed114491c451ac4281842293fdea4201ccd1fe5a
bryan <bry@payloro.com>
2024-10-23 14:29:55 +0800

    @86eqdd097 #Add property comment

--- apps/web-customer/src/components/blocks/HomePageBlocks/RevolutionBanner/RevolutionBannerBlock.tsx
+++ apps/web-customer/src/components/blocks/HomePageBlocks/RevolutionBanner/RevolutionBannerBlock.tsx
@@ -1,3 +1,8 @@
+/**
+* Property of the NCLEX Power.
+* Reuse as a whole or in part is prohibited without permission.
+* Created by the Software Strategy & Development Division
+*/
 import { ImageCarousel } from '@/components/ImageCarousel/ImageCarousel'
 import { settings } from 'core-library/core/utils/contants/wc/HomePageData'
 import { CarouselOne, CarouselTwo, CarouselThree } from 'core-library/assets'
